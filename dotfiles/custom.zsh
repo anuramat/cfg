fea6aeaafc77070510083e090e9df7a7bcfeb71f
Arsen Nuramatov <anuramat@pm.me>
2022-07-12 06:27:33 +0300

    hehe

--- dotfiles/custom.zsh
+++ dotfiles/custom.zsh
@@ -1,6 +1,13 @@
+#!/usr/bin/env zsh
+
+# requires exa, nvim, cht.sh, git
+
 alias l="exa --group-directories-first --group --icons --header --git" 
 alias v="nvim"
-export EDITOR="nvim"
 alias tree="exa --group-directories-first --group --icons --header --git --tree"
-alias githehe="git add . && git commit -m \"hehe\" && git push"
 alias c="cht.sh"
+alias githehe="git add . && git commit -m \"hehe\" && git push"
+
+export EDITOR="nvim"
+
+

c7b7b5b2541ef1e1e6d7eae9f1b44b9dabc722c7
Arsen Nuramatov <anuramat@pm.me>
2023-07-20 10:05:48 +0600

    sync

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -5,24 +5,45 @@ local u = require("utils")
 specs.dap = {
   "mfussenegger/nvim-dap",
   keys = {
-    { "<space>pp", function() require("dap").continue() end,          desc = "DAP: Continue" },
-    { "<space>pa", function() require("dap").step_over() end,         desc = "DAP: Step Over" },
-    { "<space>pb", function() require("dap").step_into() end,         desc = "DAP: Step Into" },
-    { "<space>pc", function() require("dap").step_out() end,          desc = "DAP: Step Out" },
-    { "<space>pd", function() require("dap").toggle_breakpoint() end, desc = "DAP: Toggle Breakpoint" },
+    { "<Leader>tc", function() require("dap").continue() end,          desc = "DAP: Continue" },
+    { "<Leader>tv", function() require("dap").step_over() end,         desc = "DAP: Step Over" },
+    { "<Leader>ti", function() require("dap").step_into() end,         desc = "DAP: Step Into" },
+    { "<Leader>tu", function() require("dap").step_out() end,          desc = "DAP: Step Out" },
+    { "<Leader>tt", function() require("dap").toggle_breakpoint() end, desc = "DAP: Toggle Breakpoint" },
     {
-      "<space>pf",
-      function()
-        vim.ui.input({ prompt = "Breakpoint condition:" }, function(input)
-          if input == nil then
-            return
-          end
-          require("dap").set_breakpoint(input)
-        end)
-      end,
-      desc = "DAP:  Breakpoint"
+      "<Leader>tf",
+      desc = "DAP:  Breakpoint with condition"
     },
-    { "<space>pe", function() require("dap").continue() end, desc = "DAP: Toggle Breakpoint" },
+    { '<Leader>B',  function() require('dap').set_breakpoint() end },
+    { '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end },
+    { '<Leader>dr', function() require('dap').repl.open() end },
+    { '<Leader>dl', function() require('dap').run_last() end },
+    {
+      '<Leader>th',
+      function() require('dap.ui.widgets').hover() end,
+      mode = {
+        'n', 'v' },
+    },
+    {
+      '<Leader>tp',
+      function() require('dap.ui.widgets').preview() end,
+      mode = {
+        'n', 'v' },
+    },
+    {
+      mode = 'n',
+      '<Leader>tf',
+      function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.frames)
+      end
+    },
+    {
+      '<Leader>ts', function()
+      local widgets = require('dap.ui.widgets')
+      widgets.centered_float(widgets.scopes)
+    end
+    }
   }
 }
 

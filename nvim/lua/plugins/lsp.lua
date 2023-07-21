756a1424ad77078431ec8e26daf8d8803f2e0990
Arsen Nuramatov <anuramat@pm.me>
2023-07-21 23:15:36 +0600

    sync

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -109,5 +109,16 @@ specs.lspconfig = {
   end,
 }
 
+specs.saga = {
+  'nvimdev/lspsaga.nvim',
+  event = 'LspAttach',
+  config = function()
+    require('lspsaga').setup({})
+  end,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  }
+}
 
 return u.respec(specs)

93ce47f94befb8b125a719e44911e46bca4c888f
Arsen Nuramatov <anuramat@pm.me>
2023-07-28 04:18:28 +0600

    sync

--- nvim/lua/plugins/md.lua
+++ nvim/lua/plugins/md.lua
@@ -4,7 +4,7 @@ local u = require("utils")
 
 specs.mdpreview = {
   "iamcco/markdown-preview.nvim",
-  ft = 'markdown',
+  -- ft = 'markdown',
   config = function()
     vim.fn["mkdp#util#install"]()
   end,

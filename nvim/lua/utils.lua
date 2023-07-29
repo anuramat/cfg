8323aa7db51f1cebf10b301384e53d88eff71861
Arsen Nuramatov <anuramat@pm.me>
2023-07-30 03:45:57 +0600

    sync

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -39,4 +39,20 @@ function m.values(specs)
   return result
 end
 
+--- Prints triggered events for debug purposes
+--- @param events table List of events to subscribe to
+function m.debug_events(events)
+  local g = vim.api.nvim_create_augroup("event_debugger", { clear = true })
+  local counter = 0
+  for _, e in ipairs(events) do
+    vim.api.nvim_create_autocmd(e, {
+      group = g,
+      callback = function(opts)
+        vim.notify('Event ' .. tostring(counter) .. ' triggered: ' .. opts.event)
+        counter = counter + 1
+      end,
+    })
+  end
+end
+
 return m

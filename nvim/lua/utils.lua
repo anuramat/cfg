e505ff875ef6b7798160e7d6d0337f2b07002550
Arsen Nuramatov <anuramat@pm.me>
2023-07-31 07:29:14 +0600

    sync

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -4,7 +4,7 @@ local M = {}
 --- @param s string
 --- @return boolean is_blank
 function M.is_blank(s)
-  return s:match("^%s*$") ~= nil
+  return s:match('^%s*$') ~= nil
 end
 
 --- Get indentation of the line under cursor.
@@ -12,10 +12,10 @@ end
 function M.get_indent()
   local line_i = vim.api.nvim_win_get_cursor(0)[1]                         -- Get the current line number
   local line = vim.api.nvim_buf_get_lines(0, line_i - 1, line_i, false)[1] -- Get the current line content
-  local ts = vim.api.nvim_buf_get_option(0, "tabstop")                     -- Get the current tabstop value
+  local ts = vim.api.nvim_buf_get_option(0, 'tabstop')                     -- Get the current tabstop value
 
-  local spaces = line:match("^ *") or ""
-  local tabs = line:match("^\t*") or ""
+  local spaces = line:match('^ *') or ''
+  local tabs = line:match('^\t*') or ''
 
   local indent_level = #spaces + (#tabs * ts)
   return indent_level
@@ -25,7 +25,7 @@ end
 --- @param s string
 --- @return string
 function M.trim(s)
-  return ((s:gsub("^%s+", "")):gsub("%s+$", ""))
+  return ((s:gsub('^%s+', '')):gsub('%s+$', ''))
 end
 
 --- Gets a list of values from a table.
@@ -42,7 +42,7 @@ end
 --- Prints triggered events for debug purposes
 --- @param events table List of events to subscribe to
 function M.debug_events(events)
-  local g = vim.api.nvim_create_augroup("event_debugger", { clear = true })
+  local g = vim.api.nvim_create_augroup('event_debugger', { clear = true })
   local counter = 0
   for _, e in pairs(events) do
     vim.api.nvim_create_autocmd(e, {
@@ -56,7 +56,7 @@ function M.debug_events(events)
 end
 
 --- Simulates key press
---- @param rawkey string Key, e.g. "<c-n>", "<cr>" or "K"
+--- @param rawkey string Key, e.g. '<c-n>', '<cr>' or 'K'
 function M.press(rawkey)
   local key = vim.api.nvim_replace_termcodes(rawkey, true, true, true)
   vim.api.nvim_feedkeys(key, 'n', false)

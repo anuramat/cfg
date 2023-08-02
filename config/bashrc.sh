06d0ab9ab8d3e27b44ef1ada56702cd84bec2a59
Arsen Nuramatov <anuramat@pm.me>
2023-08-03 04:19:54 +0600

    sync

--- config/bashrc.sh
+++ config/bashrc.sh
@@ -16,6 +16,21 @@ export _ZO_RESOLVE_SYMLINKS="1"
 eval "$(brew shellenv)"
 eval "$(zoxide init bash)"
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/Caskroom/miniforge/base/bin/conda' 'shell.bash' 'hook' 2>/dev/null)"
+if [ $? -eq 0 ]; then
+	eval "$__conda_setup"
+else
+	if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
+		. "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
+	else
+		export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
+	fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
 gitprompt() {
   # Check if we're in a Git repository
   if [ -d .git ] || git rev-parse --git-dir >/dev/null 2>&1; then

8cfb7f17151f14cdd98b0c1ae1c79d5f286b84e8
Arsen Nuramatov <anuramat@pm.me>
2023-08-05 17:40:45 +0600

    sync

--- setup.sh
+++ setup.sh
@@ -30,14 +30,14 @@ continue_prompt "Install fzf integration?" && {
 continue_prompt "Change shell to fish?" && {
   set_shell /opt/homebrew/bin/fish
 }
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ macOS stuff ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-screenshot_dir="$HOME/Screenshots"
-ensure_path "$screenshot_dir"
-defaults write com.apple.screencapture location -string "$screenshot_dir"
-defaults write -g ApplePressAndHoldEnabled -bool false   # allow key repeat on hold
-defaults write NSGlobalDomain AppleLanguages -array "en" # system language
-# sudo languagesetup -langspec English # login language (i've heard it doesn't work)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ iTerm2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$(realpath config/iterm2)"     # Set config path
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true                      # Autoload
 defaults write com.googlecode.iterm2 NoSyncNeverRemindPrefsChangesLostForFile_selection -int 2 # Autosave
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ macOS stuff ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+screenshot_dir="$HOME/Screenshots"
+ensure_path "$screenshot_dir"
+defaults write com.apple.screencapture location -string "$screenshot_dir"
+defaults write -g ApplePressAndHoldEnabled -bool false # allow key repeat on hold
+defaults write NSGlobalDomain AppleLanguages -array "en" system language
+sudo languagesetup -langspec English # login language

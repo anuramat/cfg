c66608024281ce5fab35d21303986ad3f6991b63
Arsen Nuramatov <anuramat@pm.me>
2023-08-08 04:07:05 +0600

    sync

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ set -e
 . ./lib/utils.sh
 . ./shell/profile.sh
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Configs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# TODO check that XDG_CONFIG_HOME is set, print paths in terminal
 export __UTILS_OVERWRITE="always"
 # Install $HOME dotfiles
 find shell -maxdepth 1 -mindepth 1 -print0 | xargs -0I{} bash -c '. lib/utils.sh; install2file {} $HOME/$(dotfilify {})'

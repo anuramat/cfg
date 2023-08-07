c133e1018997c149a54d76ef1d8d0a090a2f7fc7
Arsen Nuramatov <anuramat@pm.me>
2023-08-07 20:34:35 +0600

    sync

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ set -e
 # TODO check that XDG_CONFIG_HOME is set, print paths in terminal
 export __UTILS_OVERWRITE="always"
 # Install $HOME dotfiles
-find shell -maxdepth 1 -mindepth 1 -print0 | xargs -0I{} bash -c '. lib/utils.sh; echo {} $HOME/$(dotfilify {})'
+find shell -maxdepth 1 -mindepth 1 -print0 | xargs -0I{} bash -c '. lib/utils.sh; install2file {} $HOME/$(dotfilify {})'
 # Install $XDG_CONFIG_HOME configs
 find config -maxdepth 1 -mindepth 1 -print0 | xargs -0I{} bash -c ". lib/utils.sh; install2folder {} $XDG_CONFIG_HOME"
 # Suppress login message

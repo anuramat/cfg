f1a409ad005aaa338884af07d77c7c2cfdabe1a3
Arsen Nuramatov <anuramat@pm.me>
2023-08-07 19:07:55 +0600

    sync

--- shell/profile.sh
+++ shell/profile.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env sh
-# xdg paths
+# XDG paths
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 
-# brew path
+# Homebrew path
 export HOMEBREW_PREFIX="/opt/homebrew"
-# brew binaries are added to path separately, in rc
+eval "$("$HOMEBREW_PREFIX/bin/brew" shellenv)" # brew env variables
 
-# go modules and binaries
+# Go modules and binaries
 export GOPATH="$HOME/go"
 export PATH="$GOPATH/bin${PATH:+:$PATH}"
 
-# locale
+# Locale
 export LC_ALL="en_US.UTF-8"
 
-# editor
+# Editor
 if command -v "nvim" >/dev/null 2>&1; then
-  export EDITOR="nvim"
+	export EDITOR="nvim"
 else
-  export EDITOR="vi"
+	export EDITOR="vi"
 fi
 export VISUAL="$EDITOR"
 
-# pretty manpages
+# Pretty manpages
 if command -v "bat" >/dev/null 2>&1; then
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+	export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 fi
+
+# Read ripgrep settings
+export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgreprc"

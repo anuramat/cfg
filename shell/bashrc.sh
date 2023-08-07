524150bb19ee1457afff630b777afaaac4e3e6f0
Arsen Nuramatov <anuramat@pm.me>
2023-08-07 20:58:00 +0600

    sync

--- shell/bashrc.sh
+++ shell/bashrc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2139 # That's exactly the behaviour I want
-bind 'set bell-style none' # disable annoying sound
+bind 'set bell-style none'                     # disable annoying sound
 eval "$(/opt/homebrew/bin/brew shellenv bash)" # brew env variables
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Aliases ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 default_exa="exa --group-directories-first --group --icons --header --git --color=always"
@@ -12,19 +12,19 @@ alias tree="$default_exa --tree"
 alias bathelp="bat --plain --language=help"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Prompt ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 __overprompt() {
-  local -r status=$?
-  echo                        # Separate command-output blocks
-  echo -n " "                # Offset
-  echo -n "${PWD/#$HOME/\~}" # Current working directory, with tilde abbreviation
-  # Git branch/commit hash, if any
-  if git rev-parse --git-dir >/dev/null 2>&1; then
-    local branch
-    branch=$(git branch --show-current)
-    [ -z "$branch" ] && branch="($(git rev-parse --short HEAD))"
-    echo -n " ($branch)"
-  fi
-  # Return code, if non-zero
-  [ "$status" -ne 0 ] && echo -n " [$status]"
+	local -r status=$?
+	echo                       # Separate command-output blocks
+	echo -n " "                # Offset
+	echo -n "${PWD/#$HOME/\~}" # Current working directory, with tilde abbreviation
+	# Git branch/commit hash, if any
+	if git rev-parse --git-dir >/dev/null 2>&1; then
+		local branch
+		branch=$(git branch --show-current)
+		[ -z "$branch" ] && branch="($(git rev-parse --short HEAD))"
+		echo -n " ($branch)"
+	fi
+	# Return code, if non-zero
+	[ "$status" -ne 0 ] && echo -n " [$status]"
 }
 PS1='$(__overprompt)\n '
 PS2='│'
@@ -38,19 +38,19 @@ export _ZO_RESOLVE_SYMLINKS="1"
 [ -f ~/.fzf.bash ] && . "$HOME/.fzf.bash"
 # Fzf
 export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
-export FZF_ALT_C_OPTS="--preview 'tree {}'" 
-# Exa 
+export FZF_ALT_C_OPTS="--preview 'tree {}'"
+# Exa
 export EXA_COLORS="uu=36:gu=37:sn=32:sb=32:da=34:ur=34:uw=35:ux=36:ue=36:gr=34:gw=35:gx=36:tr=34:tw=35:tx=36"
 # Zoxide
 eval "$(zoxide init bash --cmd j)"
 # Conda
 if __conda_setup="$('/opt/homebrew/Caskroom/miniforge/base/bin/conda' 'shell.bash' 'hook' 2>/dev/null)"; then
-  eval "$__conda_setup"
+	eval "$__conda_setup"
 else
-  if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
-    . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
-  else
-    export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
-  fi
+	if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
+		. "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
+	else
+		export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
+	fi
 fi
 unset __conda_setup

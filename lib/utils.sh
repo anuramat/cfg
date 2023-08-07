e9e4c69539225c0f604724c99525a11691beb5cc
Arsen Nuramatov <anuramat@pm.me>
2023-08-07 20:30:33 +0600

    sync

--- lib/utils.sh
+++ lib/utils.sh
@@ -2,106 +2,111 @@
 # Uses __UTILS_OVERWRITE variable ("always"/any)
 
 ensure_path() {
-  # $1 -- target path
-  local -r target="$1"
+	# $1 -- target path
+	local -r target="$1"
 
-  [ -d "$target" ] && return
-  mkdir -p "$target" || {
-    echo "[cfg.fail] create path \"$target\""
-    return 1
-  }
-  echo "[cfg.write] created path \"$1\""
+	[ -d "$target" ] && return
+	mkdir -p "$target" || {
+		echo "[cfg.fail] create path \"$target\""
+		return 1
+	}
+	echo "[cfg.write] created path \"$1\""
 }
 
 ensure_string() {
-  # $1 -- string
-  # $2 -- target file
-  local -r string="$1"
-  local -r target="$2"
+	# $1 -- string
+	# $2 -- target file
+	local -r string="$1"
+	local -r target="$2"
 
-  grep -Fq "$string" "$target" && return
-  ensure_path "$(dirname "$target")" || return 1
-  echo "$string" >>"$target" || {
-    echo "[cfg.fail] append string \"$string\" to $target"
-    return 1
-  }
-  echo "[cfg.write] appended \"$string\" to $target"
+	grep -Fq "$string" "$target" && return
+	ensure_path "$(dirname "$target")" || return 1
+	echo "$string" >>"$target" || {
+		echo "[cfg.fail] append string \"$string\" to $target"
+		return 1
+	}
+	echo "[cfg.write] appended \"$string\" to $target"
 }
 
 make_symlink() {
-  # $1 -- source file
-  # $2 -- target file/directory
-  local -r original="$(realpath "$1")"
-  local target="$2"
+	# $1 -- source file
+	# $2 -- target file/directory
+	local -r original="$(realpath "$1")"
+	local target="$2"
 
-  [ -d "$target" ] && target="$target/$(basename "$original")"
-  [ -e "$target" ] && { try_overwrite "$target" || return 1; }
-  ln -sf "$original" "$target" || {
-    echo "[cfg.fail] make symlink @ \"$target\""
-    return 1
-  }
-  echo "[cfg.write] created symlink @ \"$target\""
+	[ -d "$target" ] && target="$target/$(basename "$original")"
+	[ -e "$target" ] && { try_overwrite "$target" || return 1; }
+	ln -sf "$original" "$target" || {
+		echo "[cfg.fail] make symlink @ \"$target\""
+		return 1
+	}
+	echo "[cfg.write] created symlink @ \"$target\""
 }
 
 install2folder() {
-  local -r original="$(realpath "$1")"
-  local -r target_dir="$2"
+	local -r original="$(realpath "$1")"
+	local -r target_dir="$2"
 
-  [ -e "$1" ] || {
-    echo "[cfg.fail] file \"$1\" not found!"
-    return 1
-  }
+	[ -e "$1" ] || {
+		echo "[cfg.fail] file \"$1\" not found!"
+		return 1
+	}
 
-  ensure_path "$target_dir" || return 1
-  install2file "$original" "$target_dir"
+	ensure_path "$target_dir" || return 1
+	install2file "$original" "$target_dir"
 }
 
 install2file() {
-  # $1 -- source file
-  # $2 -- target file
-  local -r original="$(realpath "$1")"
-  local -r target="$2"
-  local -r target_dir="$(dirname "$2")"
+	# $1 -- source file
+	# $2 -- target file
+	local -r original="$(realpath "$1")"
+	local -r target="$2"
+	local -r target_dir="$(dirname "$2")"
 
-  [ -e "$1" ] || {
-    echo "[cfg.fail] file \"$1\" not found!"
-    return 1
-  }
+	[ -e "$1" ] || {
+		echo "[cfg.fail] file \"$1\" not found!"
+		return 1
+	}
 
-  ensure_path "$target_dir" || return 1
-  make_symlink "$original" "$target" || return 1
+	ensure_path "$target_dir" || return 1
+	make_symlink "$original" "$target" || return 1
 }
 
 set_shell() {
-  local -r shell="$(dirname "$1")/$(basename "$1")"
+	local -r shell="$(dirname "$1")/$(basename "$1")"
 
-  [ "$SHELL" = "$shell" ] && return
-  [ -f "$shell" ] || {
-    echo "[cfg.fail] shell \"$shell\" not found"
-    return 1
-  }
-  sudo bash -c "$(declare -f ensure_path); $(declare -f ensure_string); ensure_string \"$shell\" /etc/shells"
-  chsh -s "$shell"
+	[ "$SHELL" = "$shell" ] && return
+	[ -f "$shell" ] || {
+		echo "[cfg.fail] shell \"$shell\" not found"
+		return 1
+	}
+	sudo bash -c "$(declare -f ensure_path); $(declare -f ensure_string); ensure_string \"$shell\" /etc/shells"
+	chsh -s "$shell"
 }
 
 continue_prompt() {
-  local -r prompt="$1"
-  while true; do
-    read -rp "$prompt (y/n): " choice
-    case "$choice" in
-    y | Y)
-      return 0
-      ;;
-    *)
-      return 1
-      ;;
-    esac
-  done
+	local -r prompt="$1"
+	while true; do
+		read -rp "$prompt (y/n): " choice
+		case "$choice" in
+		y | Y)
+			return 0
+			;;
+		*)
+			return 1
+			;;
+		esac
+	done
 }
 
 try_overwrite() {
-  # $1 -- target
-  local -r target="$1"
-  [ "$__UTILS_OVERWRITE" = "always" ] || continue_prompt "overwrite \"$target\"?" || return 1
-  rm -rf "$target" || return 1
+	# $1 -- target
+	local -r target="$1"
+	[ "$__UTILS_OVERWRITE" = "always" ] || continue_prompt "overwrite \"$target\"?" || return 1
+	rm -rf "$target" || return 1
+}
+
+dotfilify() {
+	# $1 -- original name/path
+	basename "$1" | perl -pe 's/(.*)\..*/.\1/'
 }
--- setup.sh
+++ setup.sh
@@ -4,29 +4,26 @@ set -e
 . ./shell/profile.sh
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Configs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # TODO check that XDG_CONFIG_HOME is set, print paths in terminal
-export __UTILS_OVERWRITE="always" # WARNING will overwrite your files without a prompt, unless they are protected
-continue_prompt "Install configs?" && {
-  install2file shell/bashrc.sh "$HOME/.bashrc"
-  install2file shell/profile.sh "$HOME/.profile"
-  # file.ext -> .file
-  find shell -maxdepth 1 -mindepth 1 -print0 | xargs -n1 -0I{} bash -c '. lib/utils.sh; basename {} | xargs -I# echo {} $HOME/$(basename {} | perl -pe "s/(.*)\..*/.\1/")' 
-  find config -maxdepth 1 -mindepth 1 -print0 | xargs -n1 -0I{} bash -c ". lib/utils.sh; install2folder {} $XDG_CONFIG_HOME"
-}
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Shell ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# bash
-ensure_string 'source "$HOME/.bashrc"' "$HOME/.bash_profile"
-# common
+export __UTILS_OVERWRITE="always"
+# Install $HOME dotfiles
+find shell -maxdepth 1 -mindepth 1 -print0 | xargs -0I{} bash -c '. lib/utils.sh; echo {} $HOME/$(dotfilify {})'
+# Install $XDG_CONFIG_HOME configs
+find config -maxdepth 1 -mindepth 1 -print0 | xargs -0I{} bash -c ". lib/utils.sh; install2folder {} $XDG_CONFIG_HOME"
+# Suppress login message
 ensure_string "hehe" "$HOME/.hushlogin"
+# Make bash the default shell
+[ "$SHELL" != "$HOMEBREW_PREFIX/bin/bash" ] && {
+	continue_prompt "Change shell to bash?" && {
+		set_shell "$HOMEBREW_PREFIX/bin/bash"
+	}
+}
+# Install fzf shell integration
 continue_prompt "Install fzf integration?" && {
-  "$HOMEBREW_PREFIX/opt/fzf/install"
+	"$HOMEBREW_PREFIX/opt/fzf/install"
 }
-# set shell
-continue_prompt "Change shell to bash?" && {
-  set_shell "$HOMEBREW_PREFIX/bin/bash"
-}
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ iTerm2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$XDG_CONFIG_HOME/iterm2"     # Set config path
+# iTerm2
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$XDG_CONFIG_HOME/iterm2"       # Set config path
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true                      # Autoload
 defaults write com.googlecode.iterm2 NoSyncNeverRemindPrefsChangesLostForFile_selection -int 2 # Autosave
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ System ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# System settings
 ./bin/macos.sh

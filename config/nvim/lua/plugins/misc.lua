58de0c4a8739a5fea41e129b584472bb40184664
Arsen Nuramatov <anuramat@pm.me>
2023-08-10 19:13:28 +0600

    sync

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -112,30 +112,6 @@ specs.readline = {
   keys = k.readline
 }
 
-specs.mdpreview = {
-  'iamcco/markdown-preview.nvim',
-  init = function()
-    local function load_then_exec(cmd)
-      return function()
-        vim.cmd.delcommand(cmd)
-        vim.cmd('Lazy load markdown-preview.nvim')
-        vim.api.nvim_exec_autocmds('BufEnter', {}) -- commands appear only after BufEnter
-        vim.cmd(cmd)
-      end
-    end
-    for _, cmd in pairs({ 'MarkdownPreview', 'MarkdownPreviewStop', 'MarkdownPreviewToggle' }) do
-      vim.api.nvim_create_user_command(cmd, load_then_exec(cmd), {})
-    end
-  end,
-  build = function()
-    vim.fn['mkdp#util#install']()
-  end,
-  config = function()
-    vim.g.mkdp_auto_close = false
-    vim.g.mkdp_echo_preview_url = true
-    vim.g.mkdp_page_title = '${name}'
-  end,
-}
 
 specs.align = {
   'echasnovski/mini.align',

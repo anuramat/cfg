edc5112f43643ca24e13863134ac32de409a8a02
Arsen Nuramatov <anuramat@pm.me>
2023-08-11 18:58:49 +0600

    sync

--- home/bashrc.sh
+++ home/bashrc.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# import blesh
+# Import blesh
 blesh_path="${XDG_DATA_HOME}/blesh/ble.sh"
 [ -r "${blesh_path}" ] && source "${blesh_path}" --noattach
 
-# basic bash specific stuff
+# Basic bash specific stuff
 bind 'set bell-style none' # Disable annoying sound
 shopt -s globstar
 
@@ -18,7 +18,7 @@ alias tree="${exa} --tree"
 alias fd="fd -H"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-# colorizes if possible
+# Colorizes a string (if possible)
 __colorize() {
 	# $1, $2, $3 - RGB
 	# $4 - text
@@ -26,31 +26,49 @@ __colorize() {
 	printf "%s" "$4"
 }
 
-# draw the prompt
-__overprompt() {
+# Draws the prompt
+__print_prompt() {
+	# Capture previous return code
 	local -r status=$?
-	# Current working directory, with tilde abbreviation
+
+	# Set up colorizers
+	local -r purple="__colorize 189 147 249"
+	local -r red="__colorize 255 85 85"
+	local -r pink="__colorize 255 121 198"
+
+	# Block divider
 	echo
-	__colorize 189 147 249 " ${PWD/#${HOME}/\~}"
-	# Git branch/commit hash, if any
+
+	# CWD
+	${purple} " ${PWD/#${HOME}/\~}"
+
+	# Git
 	if git rev-parse --git-dir >/dev/null 2>&1; then
-		local branch
-		branch=$(git branch --show-current)
+		# Print branch
+		local branch=$(git branch --show-current)
 		[ -z "${branch}" ] && branch="($(git rev-parse --short HEAD))"
-		__colorize 255 121 198 " (${branch})"
+		${pink} " (${branch})"
+
+		# Print git status
+		local -r git_status="$(git status --porcelain | while read -r line; do
+			echo "${line}" | awk '{print $1}'
+		done | tr -d '\n' | sed 's/./&\n/g' | sort | uniq | tr -d '\n')"
+		[ "${git_status}" ] && ${pink} " [${git_status}]"
 	fi
+
 	# Return code, if non-zero
-	[ "${status}" -ne 0 ] && __colorize 255 85 85 " [${status}]"
+	[ "${status}" -ne 0 ] && ${red} " [${status}]"
 	printf "\n "
 }
+PS1='$(__print_prompt)' && PS2='│'
 
-PS1='$(__overprompt)' && PS2='│'
-
+# Completion
 [ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ] && . "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
 
 # TODO add zoxide-fzf options
 command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init bash --cmd j)"
 
+# fzf
 [ -f ~/.fzf.bash ] && {
 	# . "${HOME}/.fzf.bash" # uncomment if no blesh
 	ble-import -d integration/fzf-completion
@@ -70,4 +88,5 @@ fi
 unset __conda_setup
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+# ble.sh
 [ "${BLE_VERSION}" ] && ble-attach

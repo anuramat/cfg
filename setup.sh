e2e5c823564b0502cf9f2ab6e38c11aff367f147
Arsen Nuramatov <anuramat@pm.me>
2023-08-16 16:29:52 +0600

    sync

--- setup.sh
+++ setup.sh
@@ -25,6 +25,6 @@ ensure_string "hehe" "${HOME}/.hushlogin"
 # iTerm2
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${XDG_CONFIG_HOME}/iterm2"     # Set config path
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true                      # Autoload
-defaults write com.googlecode.iterm2 NoSyncNeverRemindPrefsChangesLostForFile_selection -int 2 # Autosave
+defaults write com.googlecode.iterm2 NoSyncNeverRemindPrefsChangesLostForFile_selection -int 1 # Manual save
 # System settings
 ./bin/prefs.sh

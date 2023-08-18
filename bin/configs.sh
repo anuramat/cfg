15b94600aec840718f4d5e68454d3a7193644958
Arsen Nuramatov <anuramat@pm.me>
2023-08-18 13:17:22 +0600

    sync

--- bin/configs.sh
+++ bin/configs.sh
@@ -7,8 +7,6 @@ for __dotfile in home/*; do
 	install2file "${__dotfile}" "${HOME}/$(rehide_name "${__dotfile}")"
 done
 
-install2file "./config/nvim/init.vim" "${HOME}/.vimrc"
-
 # Install $XDG_CONFIG_HOME configs
 for __folder in config/*; do
 	install2folder "${__folder}" "${XDG_CONFIG_HOME}"

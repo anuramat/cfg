3167c4315ac6c023be3c5343b22bba471409c544
Arsen Nuramatov <anuramat@pm.me>
2023-08-19 02:17:58 +0600

    sync

--- dump.sh
+++ dump.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 NEW_BREWFILE="new.Brewfile"
-brew bundle dump --file "${NEW_BREWFILE}"
+brew bundle dump --file "${NEW_BREWFILE}" --force
 comm -13 <(sort Brewfile) <(sort "${NEW_BREWFILE}") >brewdiff

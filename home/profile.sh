6b1c5b6d666440ab9f585af0ae044c10955f4c98
Arsen Nuramatov <anuramat@pm.me>
2023-08-26 02:42:19 +0600

    sync

--- home/profile.sh
+++ home/profile.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/env sh
 
 # XDG paths
-export XDG_CONFIG_HOME="${HOME}/.config"
-export XDG_CACHE_HOME="${HOME}/.cache"
-export XDG_DATA_HOME="${HOME}/.local/share"
-export XDG_STATE_HOME="${HOME}/.local/state"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
 
 # Locale
 export LC_ALL="en_US.UTF-8"
 
 # Homebrew path
 export HOMEBREW_PREFIX="/opt/homebrew"
-eval "$("${HOMEBREW_PREFIX}/bin/brew" shellenv)" # brew env variables
+if _brewshellenv="$("$HOMEBREW_PREFIX/bin/brew" shellenv)"; then
+	eval "$_brewshellenv"
+fi
 
 # Editor
 for editor in nvim vim nvi vi; do
-	if command -v "${editor}" >/dev/null 2>&1; then
-		VISUAL="${editor}"
+	if command -v "$editor" >/dev/null 2>&1; then
+		VISUAL="$editor"
 		break
 	fi
 done
 export VISUAL
-export EDITOR="${VISUAL}"
+export EDITOR="$VISUAL"
 
 # Go modules and binaries
-export GOPATH="${HOME}/go"
-export PATH="${GOPATH}/bin${PATH:+:${PATH}}"
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin${PATH:+:$PATH}"
 
 # __pycache__ folder
 export PYTHONPYCACHEPREFIX="/tmp/pycache"

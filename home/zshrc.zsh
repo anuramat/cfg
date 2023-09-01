d5807be323577f5135fd949bf2c7abfe949ac9e6
Arsen Nuramatov <anuramat@pm.me>
2023-09-01 06:51:51 +0600

    sync

--- home/zshrc.zsh
+++ home/zshrc.zsh
@@ -1,13 +1,15 @@
 #!/usr/bin/env zsh
+# best effort support
 
 setopt PROMPT_SUBST                    # so that the bash PS1 works
 
 . "${HOME}/.profile"                   # basic env vars
 . "${XDG_CONFIG_HOME}/bash/utils.sh"   # helper functions
 . "${XDG_CONFIG_HOME}/bash/opts.sh"    # options for external tools
-. "${XDG_CONFIG_HOME}/bash/aliases.sh" #
-. "${XDG_CONFIG_HOME}/bash/prompt.sh"  #
+. "${XDG_CONFIG_HOME}/bash/aliases.sh"
+. "${XDG_CONFIG_HOME}/bash/prompt.sh"
 [ -r "$XDG_CONFIG_HOME/bash/private.sh" ] && . "$XDG_CONFIG_HOME/bash/private.sh"
 
+# TODO conda
 [ -f ~/.fzf.zsh ] && . ~/.fzf.zsh
 command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init zsh --cmd j --hook pwd)"

d0f07ecfc17a1e1d31c498479a0118f9cd653014
Arsen Nuramatov <anuramat@pm.me>
2023-09-10 21:56:57 +0000

    sync

--- config/bash/prompt.sh
+++ config/bash/prompt.sh
@@ -3,84 +3,95 @@
 # Don't let python venvs change the PS1
 export VIRTUAL_ENV_DISABLE_PROMPT="1"
 
-# Colorizes a string (if possible)
-__colorize() {
+# Converts RGB values to 24-bit color escape code
+__colorcode_from_rgb() {
 	# $1, $2, $3 - RGB
-	# $4 - text
 	[ "$(tput colors)" -eq 256 ] && printf "\033[38;2;%s;%s;%sm" "$1" "$2" "$3"
 }
 
+# Goes through "git status --porcelain=v1" output, searching for the letter
+__git_status_attr() {
+	# $1 - target letter
+	# $2 - output symbol (defautls to $1)
+	grep -q -e "^.$1" -e "^$1." && {
+		[ "$2" ] && printf "$2" && return
+		printf "$1"
+	}
+}
+
+# Returns git status string of the form '[A-Z]+'
+__git_status() {
+	# $1 - root dir
+	local porcelain=$(git -C "$1" status --porcelain)
+	printf "$(echo "$porcelain" | __git_status_attr A)" # A: addition of a file
+	printf "$(echo "$porcelain" | __git_status_attr C)" # C: copy of a file into a new one
+	printf "$(echo "$porcelain" | __git_status_attr D)" # D: deletion of a file
+	printf "$(echo "$porcelain" | __git_status_attr M)" # M: modification of the contents or mode of a file
+	printf "$(echo "$porcelain" | __git_status_attr R)" # R: renaming of a file
+	printf "$(echo "$porcelain" | __git_status_attr T)" # T: change in the type of the file
+	printf "$(echo "$porcelain" | __git_status_attr U)" # U: file is unmerged (you must complete the merge before it can be committed)
+	printf "$(echo "$porcelain" | __git_status_attr X)" # X: "unknown" change type (most probably a bug, please report it)
+}
+
 __git_prompt() {
+	printf "$__pink"
 	local git_dir
 	if git_dir="$(git rev-parse --git-dir 2>/dev/null)"; then
 		git_dir="$(realpath "$git_dir")"
+		local -r root_dir="$(dirname "$git_dir")"
+		local -r sep=':'
+		printf ' '
 
-		# Bare repository
+		# Bare repository case
 		if [ "$(git rev-parse --is-bare-repository 2>/dev/null)" = "true" ]; then
 			printf "$(basename -s .git "$git_dir")"
 			return
 		fi
 
-		local -r root_dir="$(realpath "$git_dir"/..)"
-
 		# Repository name
 		local -r url="$(git config --get remote.origin.url)"
-		local -r __path="$(dirname "$git_dir")"
-		local -r repo_name="$(basename -s .git "${url:-$__path}")"
+		local -r repo_name="$(basename -s .git "${url:-$root_dir}")"
 		printf "$repo_name"
 
 		# Branch
 		local branch="$(git branch --show-current)"
 		[ -z "$branch" ] && branch="$(git -C "$root_dir" rev-parse --short HEAD)"
-		printf ":$branch"
+		printf "$sep$branch"
 
-		# # Status
-		# TODO make this faster
-		# local git_status="$(git -C "$root_dir" status --porcelain | while read -r line; do
-		# 	echo "$line" | awk '{print $1}'
-		# done | tr -d '\n' | sed 's/./&\n/g' | sort | uniq | tr -d '\n')"
-		# [ "$git_status" ] && printf ":$git_status"
+		# Status
+		local -r git_status="$(__git_status "$root_dir")"
+		[ "$git_status" ] && printf "$sep$git_status"
 
 		# TODO Unpushed commits
 	fi
+	printf "$__norm"
 }
 
 # Set up colors
 # https://spec.draculatheme.com/
-__green=$(__colorize 80 250 123)
-__purple=$(__colorize 189 147 249)
-__red=$(__colorize 255 85 85)
-__pink=$(__colorize 255 121 198)
+__green=$(__colorcode_from_rgb 80 250 123)
+__purple=$(__colorcode_from_rgb 189 147 249)
+__red=$(__colorcode_from_rgb 255 85 85)
+__pink=$(__colorcode_from_rgb 255 121 198)
 __bold="\033[1m"
 __norm="\033[0m"
 
 # Draws the prompt
-__prompt() {
-	# Capture previous return code
-	local -r __status=$?
-
-	# Block divider
-	echo
-
-	# CWD
-	printf " $__bold$__purple%s$__norm" "${PWD/#$HOME/"~"}"
-	# TODO use sed to make it POSIX?
-
-	# Git
-	printf "$__pink "
-	__git_prompt
-	printf "$__norm"
-
-	# Conda
-	[ "$CONDA_DEFAULT_ENV" ] && printf " $__greenconda:%s$__norm" "$CONDA_DEFAULT_ENV"
-	# Python venv
-	# TODO make it "$parentdirname/$venvdirname"
-	[ "$VIRTUAL_ENV" ] && printf " $__greenvenv:%s$__norm" "$(basename "$(dirname "$VIRTUAL_ENV")")"
-
-	# Return code
-	[ "$__status" -ne 0 ] && printf " $__bold$__red%s$__norm" "$__status"
-
-	printf "\n "
+__python_prompt() {
+	local sep=' '
+	# conda
+	[ "$CONDA_DEFAULT_ENV" ] && printf "$sep${__green}conda:%s$__norm" "$CONDA_DEFAULT_ENV" && sep=' '
+	# venv
+	[ "$VIRTUAL_ENV" ] && printf "$sep${__green}venv:%s$__norm" "$VIRTUAL_ENV" && sep=' '
 }
-PS1='$(__prompt)'
+
+__return_code_prompt() {
+	[ "$__last_return_code" -ne 0 ] && printf " $__bold$__red$__last_return_code$__norm"
+	printf "$__norm"
+}
+
+__path="$__bold$__purple\w$__norm"
+
+PROMPT_COMMAND='__last_return_code=$?' # Capture last return code
+PS1="\n $__path\$(__git_prompt)\$(__python_prompt)\$(__return_code_prompt)\n "
 PS2='│'

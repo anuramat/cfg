19d970e6cc7cebdd8ef78d171cfd48beef1301ad
Arsen Nuramatov <anuramat@pm.me>
2023-09-10 23:00:58 +0600

    sync

--- config/bash/opts.sh
+++ config/bash/opts.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env bash
 # ~~~~~~~~~~~~~~~~~~~~~~~~~ fzf ~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-export FZF_DEFAULT_OPTS="\
---bind 'ctrl-/:change-preview-window(down|hidden|)' \
---preview '$XDG_CONFIG_HOME/bash/fzf_previewer.sh {}'
-"
-
+# replace find
 if command -v "fd" >/dev/null 2>&1; then
 	export FZF_DEFAULT_COMMAND="fd ."
 	export FZF_ALT_C_COMMAND="fd -t d ."
 fi
-# ~~~~~~~~~~~~~~~~~~~~~~~~ zoxide ~~~~~~~~~~~~~~~~~~~~~~~~ #
-# TODO prettify
-export _ZO_RESOLVE_SYMLINKS="1"
+# default opts
+export FZF_DEFAULT_OPTS=" \
+--preview='$XDG_CONFIG_HOME/bash/fzf_previewer.sh {}' \
+--layout=reverse \
+--keep-right \
+--info=inline \
+--tabstop=2 \
+--height=50% \
+\
+--bind='ctrl-/:change-preview-window(down|hidden|)' \
+--bind='ctrl-j:jump-accept' \
+"
+# zoxide
 __zo_fzf_preview='ls --color=always -Cp'
 if command -v "exa" >/dev/null 2>&1; then
-	__zo_fzf_preview='exa --group-directories-first --icons'
+	__zo_fzf_preview='exa --group-directories-first --icons --grid'
 fi
-export _ZO_FZF_OPTS="\
+export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS \
 --no-sort \
---bind=ctrl-z:jump,btab:up,tab:down \
---cycle \
---keep-right \
---border=sharp \
---height=45% \
---info=inline \
---layout=reverse \
---tabstop=1 \
 --exit-0 \
 --select-1 \
---preview-window=down,30%,sharp \
---preview='$__zo_fzf_preview {2..}' \
+--preview='$__zo_fzf_preview {2..}'
 "
+# ~~~~~~~~~~~~~~~~~~~~~~~~ zoxide ~~~~~~~~~~~~~~~~~~~~~~~~ #
+export _ZO_RESOLVE_SYMLINKS="1"
 # ~~~~~~~~~~~~~~~~~~~~ read ripgreprc ~~~~~~~~~~~~~~~~~~~~ #
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgreprc"

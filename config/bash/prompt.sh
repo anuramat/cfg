cd2956b4a4dfd478f0e827cabfba2b0790421064
Arsen Nuramatov <anuramat@pm.me>
2023-09-11 00:15:29 +0600

    sync

--- config/bash/prompt.sh
+++ config/bash/prompt.sh
@@ -4,14 +4,13 @@
 export VIRTUAL_ENV_DISABLE_PROMPT="1"
 
 # Converts RGB values to 24-bit color escape code
-__colorize() {
+__colorcode_from_rgb() {
 	# $1, $2, $3 - RGB
-	# $4 - text
 	[ "$(tput colors)" -eq 256 ] && printf "\033[38;2;%s;%s;%sm" "$1" "$2" "$3"
 }
 
 # Goes through "git status --porcelain=v1" output, searching for the letter
-__grep_porce_letter() { # TODO naming (prob everywhere in this file)
+__git_status_attr() { # TODO naming (prob everywhere in this file)
 	# $1 - target letter
 	# $2 - output symbol (defautls to $1)
 	grep -q -e "^.$1" -e "^$1." && {
@@ -20,26 +19,28 @@ __grep_porce_letter() { # TODO naming (prob everywhere in this file)
 	}
 }
 
+# Returns git status string of the form '[A-Z]+'
 __git_status() {
 	# $1 - root dir
 	local porcelain=$(git -C "$1" status --porcelain)
-	printf "$(echo "$porcelain" | __grep_porce_letter A)" # A: addition of a file
-	printf "$(echo "$porcelain" | __grep_porce_letter C)" # C: copy of a file into a new one
-	printf "$(echo "$porcelain" | __grep_porce_letter D)" # D: deletion of a file
-	printf "$(echo "$porcelain" | __grep_porce_letter M)" # M: modification of the contents or mode of a file
-	printf "$(echo "$porcelain" | __grep_porce_letter R)" # R: renaming of a file
-	printf "$(echo "$porcelain" | __grep_porce_letter T)" # T: change in the type of the file
-	printf "$(echo "$porcelain" | __grep_porce_letter U)" # U: file is unmerged (you must complete the merge before it can be committed)
-	printf "$(echo "$porcelain" | __grep_porce_letter X)" # X: "unknown" change type (most probably a bug, please report it)
+	printf "$(echo "$porcelain" | __git_status_attr A)" # A: addition of a file
+	printf "$(echo "$porcelain" | __git_status_attr C)" # C: copy of a file into a new one
+	printf "$(echo "$porcelain" | __git_status_attr D)" # D: deletion of a file
+	printf "$(echo "$porcelain" | __git_status_attr M)" # M: modification of the contents or mode of a file
+	printf "$(echo "$porcelain" | __git_status_attr R)" # R: renaming of a file
+	printf "$(echo "$porcelain" | __git_status_attr T)" # T: change in the type of the file
+	printf "$(echo "$porcelain" | __git_status_attr U)" # U: file is unmerged (you must complete the merge before it can be committed)
+	printf "$(echo "$porcelain" | __git_status_attr X)" # X: "unknown" change type (most probably a bug, please report it)
 }
 
-__git() {
+__git_prompt() {
 	printf "$__pink"
 	local git_dir
 	if git_dir="$(git rev-parse --git-dir 2>/dev/null)"; then
 		git_dir="$(realpath "$git_dir")"
 		local -r root_dir="$(dirname "$git_dir")"
 		local -r sep=':'
+		printf ' '
 
 		# Bare repository case
 		if [ "$(git rev-parse --is-bare-repository 2>/dev/null)" = "true" ]; then
@@ -68,30 +69,29 @@ __git() {
 
 # Set up colors
 # https://spec.draculatheme.com/
-__green=$(__colorize 80 250 123)
-__purple=$(__colorize 189 147 249)
-__red=$(__colorize 255 85 85)
-__pink=$(__colorize 255 121 198)
+__green=$(__colorcode_from_rgb 80 250 123)
+__purple=$(__colorcode_from_rgb 189 147 249)
+__red=$(__colorcode_from_rgb 255 85 85)
+__pink=$(__colorcode_from_rgb 255 121 198)
 __bold="\033[1m"
 __norm="\033[0m"
 
 # Draws the prompt
-__python() {
-	local -r sep=''
+__python_prompt() {
+	local sep=' '
 	# conda
 	[ "$CONDA_DEFAULT_ENV" ] && printf "$sep${__green}conda:%s$__norm" "$CONDA_DEFAULT_ENV" && sep=' '
 	# venv
 	[ "$VIRTUAL_ENV" ] && printf "$sep${__green}venv:%s$__norm" "$VIRTUAL_ENV" && sep=' '
 }
 
-__return_code() {
-	printf "$__bold$__red"
-	[ "$__last_return_code" -ne 0 ] && printf "$__last_return_code"
+__return_code_prompt() {
+	[ "$__last_return_code" -ne 0 ] && printf " $__bold$__red$__last_return_code$__norm"
 	printf "$__norm"
 }
 
 __path="$__bold$__purple\w$__norm"
 
 PROMPT_COMMAND='__last_return_code=$?' # Capture last return code
-PS1="\n $__path \$(__git) \$(__python) \$(__return_code)\n "
+PS1="\n $__path\$(__git_prompt)\$(__python_prompt)\$(__return_code_prompt)\n "
 PS2='│'

ff5f6172c72357f9bcc7794ac70755ecdd086b7f
Arsen Nuramatov <anuramat@pm.me>
2023-09-10 22:51:27 +0000

    sync

--- sync.sh
+++ sync.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-nixos_cfg="/etc/nixos/configuration.nix"
-[ -r  "$nixos_cfg" ] && cp "$nixos_cfg" ./
+repo_cfg="./configuration.nix"
+[ "$(git diff "$repo_cfg")" ] && echo "$repo_cfg has uncommited changed, can't proceed"
+sys_cfg="/etc/nixos/configuration.nix"
+[ -r "$sys_cfg" ] && cp "$sys_cfg" ./
 
 git add --all && git commit -am sync && git push

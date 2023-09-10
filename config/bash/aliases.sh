1dff7ff5084c1c7803523eb66b7b44a5f4ad9dfe
Arsen Nuramatov <anuramat@pm.me>
2023-09-10 19:34:55 +0000

    sync

--- config/bash/aliases.sh
+++ config/bash/aliases.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-exa="exa --group-directories-first --group --icons --header --git"
 alias f="nvim"
+
+exa="exa --group-directories-first --group --icons --header --git"
 alias ls="$exa"
 alias ll="$exa --long"
 alias la="$exa --long --all"
 alias tree="$exa --tree"
+unset exa
+
 alias fd="fd -H"
+
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
-unset exa
+
+alias nire="sudo nixos-rebuild switch"
+alias nied="sudo -e /etc/nixos/configuration.nix"

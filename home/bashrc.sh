2e33d6629ef307af2f3248a0b9048934274596c7
Arsen Nuramatov <anuramat@pm.me>
2023-09-10 21:57:55 +0000

    sync

--- home/bashrc.sh
+++ home/bashrc.sh
@@ -3,13 +3,13 @@
 bind 'set bell-style none' # Disable annoying sound
 shopt -s globstar          # Enables ** for recursing into subdirectories
 
-. "$HOME/.profile"                 # basic env vars
-. "$XDG_CONFIG_HOME/bash/utils.sh" # helper functions
-. "$XDG_CONFIG_HOME/bash/opts.sh"  # options for external tools
+. "$HOME/.profile"                # basic env vars
+. "$XDG_CONFIG_HOME/bash/opts.sh" # options for external tools
 . "$XDG_CONFIG_HOME/bash/aliases.sh"
 . "$XDG_CONFIG_HOME/bash/prompt.sh"
 [ -r "$XDG_CONFIG_HOME/bash/private.sh" ] && . "$XDG_CONFIG_HOME/bash/private.sh"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Hooks ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# TODO
 # conda_path="$HOMEBREW_PREFIX/Caskroom/miniforge/base"
 # if conda_hook="$("$conda_path/bin/conda" "shell.bash" "hook" 2>/dev/null)"; then
 # 	eval "$conda_hook"
@@ -21,8 +21,8 @@ shopt -s globstar          # Enables ** for recursing into subdirectories
 # unset conda_path conda_hook
 
 if command -v fzf-share >/dev/null; then
-  source "$(fzf-share)/key-bindings.bash"
-  source "$(fzf-share)/completion.bash"
+	source "$(fzf-share)/key-bindings.bash"
+	source "$(fzf-share)/completion.bash"
 fi
 
 command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init bash --cmd j --hook pwd)"

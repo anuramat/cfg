9d6968c3c895faec2d3f9609be7efa444f481129
Arsen Nuramatov <anuramat@pm.me>
2023-12-03 02:09:20 +0600

    sync

--- home/profile.sh
+++ home/profile.sh
@@ -5,6 +5,9 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+
+export GRIM_DEFAULT_DIR="$HOME/Screenshots"
 
 # User binaries
 export PATH="$HOME/bin${PATH:+:$PATH}"

95bd922c693b35460670c509f961cc0d5582e6b7
Arsen Nuramatov <anuramat@pm.me>
2024-02-04 22:29:58 +0600

    sync

--- up.sh
+++ up.sh
@@ -2,8 +2,12 @@
 set -e
 
 local_nixos_folder="./nixos/"
-[ "$(git diff "$local_nixos_folder")" ] && echo "$(basename "$local_nixos_folder") has uncommited changed, can't proceed" && exit
 sys_nixos_folder="/etc/nixos"
+
+# check if there's any uncommited changes in the repo
+[ "$(git diff @ -- "$local_nixos_folder")" ] && echo "$(basename "$local_nixos_folder") has uncommited changed, can't proceed" && exit
+# copy NixOS stuff to the repo
 [ -r "$sys_nixos_folder" ] && cp "$sys_nixos_folder" "$local_nixos_folder"
 
-# git add --all && git commit -am sync && git push
+# push everything
+# git commit -am sync && git push

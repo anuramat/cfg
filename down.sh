7a762ca1712f51c72058a14f3e57dbdead91c88a
Arsen Nuramatov <anuramat@pm.me>
2024-02-04 23:01:33 +0600

    sync

--- down.sh
+++ down.sh
@@ -4,9 +4,8 @@ set -e
 . ./lib/utils.sh
 . ./home/profile.sh
 
-echo "[cfg] installing configs"
-
 # Install $HOME dotfiles
+echo "[cfg] installing dotfiles"
 for __dotfile in home/*; do
 	install2file "$__dotfile" "$HOME/.$(remove_extension "$__dotfile")"
 done
@@ -14,10 +13,12 @@ done
 install2folder config/nvim/after "$HOME/.vim/"
 
 # Install $XDG_CONFIG_HOME configs
+echo "[cfg] installing configs"
 for __folder in config/*; do
 	install2folder "$__folder" "$XDG_CONFIG_HOME"
 done
 
+echo "[cfg] creating directories"
 # TODO move all of this to configuration.nix or home manager
 touch "$HOME/.hushlogin"
 ensure_path "$HOME/bin"

1cd6f55682ae71de877e7ae850d9d4f8e301609b
Arsen Nuramatov <anuramat@pm.me>
2024-02-22 07:11:59 +0600

    auto

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -19,15 +19,17 @@ end
 specs.cmp = {
   'hrsh7th/nvim-cmp',
   dependencies = {
-    'L3MON4D3/LuaSnip',
+    'L3MON4D3/LuaSnip', -- TODO use vsnip? brand loyalty xd
+    'saadparwaiz1/cmp_luasnip',
+
+    'onsails/lspkind.nvim', -- icons
+
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lua',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-emoji',
-    'saadparwaiz1/cmp_luasnip',
-    'onsails/lspkind.nvim',
   },
   config = function()
     local cmp = require('cmp')

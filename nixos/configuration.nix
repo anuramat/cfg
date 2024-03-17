d48b48c20b92c592516424bddc1285b7ecc49d7e
Arsen Nuramatov <anuramat@pm.me>
2024-03-17 05:02:11 +0500

    auto

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,8 +23,13 @@ in
 {
   imports = [
     ./hardware-configuration.nix
-    <home-manager/nixos>
+
     ./boilerplate.nix
+    ./hardware.nix
+    ./printers.nix
+    ./networking.nix
+    
+    <home-manager/nixos>
   ];
   nixpkgs.config.permittedInsecurePackages = [
     "electron-25.9.0"
@@ -113,22 +118,6 @@ in
       };
       systemPackages = import ./packages.nix pkgs unstable;
     };
-  # ~~~~~~~~~~~~~~~~~~~~~~~~~ Hardware ~~~~~~~~~~~~~~~~~~~~~~~~~
-  # ZSA Voyager
-  services.udev.extraRules = ''
-    # Rules for Oryx web flashing and live training
-    KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
-    KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
-    # Keymapp Flashing rules for the Voyager
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
-  '';
-  # Flipper Zero
-  hardware.flipperzero.enable = true;
-  # Removable media
-  services.udisks2 = {
-    enable = true;
-    mountOnMedia = true;
-  };
   # ~~~~~~~~~~~~~~~~~~~~~~ Misc software ~~~~~~~~~~~~~~~~~~~~~~~
   # TODO restic backups
   services.gnome.gnome-keyring.enable = true;
---
+++ nixos/hardware.nix
@@ -0,0 +1,18 @@
+{}:
+{
+  # ZSA Voyager
+  services.udev.extraRules = ''
+    # Rules for Oryx web flashing and live training
+    KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+    # Keymapp Flashing rules for the Voyager
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
+  '';
+  # Flipper Zero
+  hardware.flipperzero.enable = true;
+  # Removable media
+  services.udisks2 = {
+    enable = true;
+    mountOnMedia = true;
+  };
+}

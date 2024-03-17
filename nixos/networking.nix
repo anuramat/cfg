9bac476f4fe4c4f0ee0b9d8bd27e0ebad91d861c
Arsen Nuramatov <anuramat@pm.me>
2024-03-17 10:56:15 +0500

    auto

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -4,24 +4,19 @@
   networking =
     {
       firewall.enable = true;
-      # firewall.allowedTCPPorts = [ ... ];
-      # firewall.allowedUDPPorts = [ ... ];
+      firewall.allowedTCPPorts = [ ];
+      firewall.allowedUDPPorts = [ ];
       networkmanager = {
         enable = true;
-        wifi.backend = "iwd"; # autoconnect might be broken from time to time
       };
-      wireless.iwd = {
-        enable = true;
-        settings = { Settings = { AutoConnect = true; }; };
-      };
-      nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ]; # Set cloudflare dns TODO what does #one.one.one.one mean
+      nameservers = [ "1.1.1.1" "1.0.0.1" ]; # Set cloudflare dns TODO what does #one.one.one.one mean
     };
   # uses DNSSEC and DNSoverTLS, might break on a different ns
   services.resolved = {
     enable = true;
     dnssec = "true";
     domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    fallbackDns = [ "1.1.1.1" "1.0.0.1" ];
     extraConfig = ''
       DNSOverTLS=yes
     '';

01246e1b82b42daa0ba1a680bc8611333ce39e3c
Arsen Nuramatov <anuramat@pm.me>
2024-03-18 11:51:39 +0500

    auto

--- nixos/expr/mime.nix
+++ nixos/expr/mime.nix
@@ -1,29 +1,23 @@
+let
+  browser = "google-chrome.desktop";
+  filemanager = "nnn.desktop";
+  documents = "org.pwmt.zathura.desktop";
+  editor = "nvim.desktop";
+in
 {
-  "x-scheme-handler/http" = [ "google-chrome.desktop" ];
-  "x-scheme-handler/https" = [ "google-chrome.desktop" ];
-  "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
-  "x-scheme-handler/vscode" = [ "code-url-handler.desktop" ];
+  "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+  "x-scheme-handler/vscode" = "code-url-handler.desktop";
+  "x-scheme-handler/magnet" = "transmission-gtk.desktop";
 
   # all `text/*` types are subclasses of `text/plain`
-  "text/plain" = [ "nvim.desktop" ];
-  "application/x-shellscript" = [ "nvim.desktop" ];
-  # "text/x-c" = [ "nvim.desktop" ];
-  # "text/x-c++" = [ "nvim.desktop" ];
+  "text/plain" = editor;
 
-  "inode/directory" = [ "nnn.desktop" ];
+  "inode/directory" = filemanager;
+  "application/pdf" = documents;
 
-  # "application/pdf" = [
-
-  "image/gif" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/jpeg" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/png" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/bmp" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/tiff" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/x-eps" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/x-ico" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/x-portable-bitmap" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/x-portable-graymap" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/x-portable-pixmap" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/x-xbitmap" = [ "org.nomacs.ImageLounge.desktop" ];
-  "image/x-xpixmap" = [ "org.nomacs.ImageLounge.desktop" ];
+  "x-scheme-handler/http" = browser;
+  "x-scheme-handler/https" = browser;
+  "x-scheme-handler/about" = browser;
+  "x-scheme-handler/unknown" = browser;
+  "text/html" = browser;
 }

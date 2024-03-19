9e5dee524ac8d134d747f994cfacc9b18a6ea216
Arsen Nuramatov <anuramat@pm.me>
2024-03-20 03:02:08 +0500

    auto

---
+++ lib/test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+fail="$(tput setaf 1 bold)"
+ok="$(tput setaf 2 bold)"
+normal="$(tput sgr0)"
+
+check() {
+	# $1 - file mimetype
+	# $2 - check command
+	find . ! -path '*/.git/*' -print0 | while IFS= read -r -d '' file; do
+		if [ "$(file "$file" --mime-type --brief)" = "$1" ]; then
+			result="$($2 "$file")" && printf "$ok%$(tput cols)s\r$normal" "OK" || printf "$fail%$(tput cols)s\r$normal" "FAIL"
+			echo "Checking $file:"
+			[ -n "$result" ] && echo "$result"
+		fi
+	done
+}
+
+autoshell() {
+	check 'text/x-shellscript' 'shellcheck --color=always -o all'
+}
+
+posix() {
+	check 'text/x-shellscript' 'shellcheck --color=always -s sh -o all'
+}

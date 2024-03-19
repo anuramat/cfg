c206a3ce66f3cee0a2d48c951f29c457925a86d5
Arsen Nuramatov <anuramat@pm.me>
2024-03-20 04:20:43 +0500

    auto

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -16,7 +16,8 @@ return {
     -- -- add border to default hover handler (replaced by folke/noice.nvim)
     -- vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = vim.g.border })
     -- -- add border to default signature help (replaced with ray-x/lsp_signature.nvim)
-    -- vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = vim.g.border })
+    -- vim.lsp.handlers['textDocument/signatureHelp'] =
+    --   vim.lsp.with(vim.lsp.handlers.signature_help, { border = vim.g.border })
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ --
     -- Register capabilities for CMP
     local capabilities = vim.lsp.protocol.make_client_capabilities()

d90d4e6dc686dbd7f6ffa5b1ae3c79e4d80b5576
Arsen Nuramatov <anuramat@pm.me>
2024-03-19 21:24:51 +0500

    auto

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,5 @@
 local specs = {}
-local configs = require('lsp.lsp_configs')
+local configs = require('lsp.language_specific.lsp_configs')
 local on_attach = require('lsp.on_attach')
 local u = require('utils')
 

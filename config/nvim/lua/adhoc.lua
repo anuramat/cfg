974060dc6abb19c99be8b29c8b5efb8446ec8701
Arsen Nuramatov <anuramat@pm.me>
2024-03-19 21:04:44 +0500

    auto

--- config/nvim/lua/adhoc.lua
+++ config/nvim/lua/adhoc.lua
@@ -82,3 +82,19 @@ end, 'Header')
 set('<leader>$', function()
   create_comment_header('~', nil, 60)
 end, 'Subheader')
+
+--- Prints triggered events for debug purposes
+--- @param events table List of events to subscribe to
+local debug_events = function(events)
+  local g = vim.api.nvim_create_augroup('event_debugger', { clear = true })
+  local counter = 0
+  for _, e in pairs(events) do
+    vim.api.nvim_create_autocmd(e, {
+      group = g,
+      callback = function(opts)
+        vim.notify('Event ' .. tostring(counter) .. ' triggered: ' .. opts.event)
+        counter = counter + 1
+      end,
+    })
+  end
+end

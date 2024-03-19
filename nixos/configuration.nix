da851b1dd9f4b0f192ac7ccac81ae88cbf711eb1
Arsen Nuramatov <anuramat@pm.me>
2024-03-19 16:14:20 +0500

    auto

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,6 +19,7 @@ in
     ./modules/boilerplate.nix
     ./modules/peripherals.nix
     ./modules/overlays.nix
+    ./modules/sound.nix
 
     <home-manager/nixos>
   ];
@@ -33,19 +34,4 @@ in
   home-manager.users.${user.username} = {
     home.stateVersion = user.stateVersion;
   };
-  # ~~~~~~~~~~~~~~~~~~~~~~~~~~ Sound ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # https://nixos.wiki/wiki/PipeWire
-  services.pipewire = {
-    # TODO some of these aren't needed probably
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-  # "sound.enable is only meant for ALSA-based configurations"
-  sound.enable = false;
-  # RealtimeKit, scheduling priotity of user processes, used eg by PulseAudio to get realtime priority
-  # "optional but recommended"
-  security.rtkit.enable = true;
 }

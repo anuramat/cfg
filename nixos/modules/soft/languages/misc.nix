ef9cad6e18f3e32ac737beb657b337b4357fcb18
Arsen Nuramatov <anuramat@pm.me>
2024-03-20 10:39:57 +0500

    auto

--- nixos/modules/soft/languages/misc.nix
+++ nixos/modules/soft/languages/misc.nix
@@ -3,5 +3,10 @@
     jq # json processor
     yq # basic yaml, json, xml, csv, toml processor
     bats # Bash testing
+    universal-ctags # maintained ctags
+    bear # Compilation database generator for clangd
+    luajitPackages.luarocks
+    # micromamba # conda rewrite in C++ # doesn't fucking work
+    # conda
   ];
 }

1265dcd858b2925f7555bc6c70393cf3cdc91fbc
Arsen Nuramatov <anuramat@pm.me>
2024-03-21 10:23:29 +0500

    auto

--- config/nvim/after/ftplugin/gitcommit.lua
+++
@@ -1,9 +0,0 @@
-local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, true)[1]
-if first_line == '' then
-  local branch = vim.fn.system({ 'git', 'branch', '--show-current' })
-  local ticket = string.match(branch, '%u+-%d+')
-  if ticket ~= nil then
-    local template = '[' .. ticket .. ']'
-    vim.api.nvim_buf_set_lines(0, 0, 1, true, { template })
-  end
-end

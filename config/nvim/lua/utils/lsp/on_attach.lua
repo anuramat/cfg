08a02df1de069f8f761cdaed293b8ffe891c57d5
Arsen Nuramatov <anuramat@pm.me>
2024-03-24 02:50:06 +0500

    auto

--- config/nvim/lua/utils/lsp/on_attach.lua
+++ config/nvim/lua/utils/lsp/on_attach.lua
@@ -4,5 +4,6 @@
 return function(client, buffer)
   require('utils.lsp.keys').setup_lsp_keybinds(buffer)
   require('utils.lsp.formatting').setup_lsp_autoformatting(client, buffer)
+  vim.lsp.inlay_hint.enable()
   -- require('lsp-format').on_attach(client, buffer)
 end

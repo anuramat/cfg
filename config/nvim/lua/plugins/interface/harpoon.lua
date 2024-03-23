6673fbd376eb010de6cef16d9814081b5df888e0
Arsen Nuramatov <anuramat@pm.me>
2024-03-23 20:51:09 +0500

    auto

--- config/nvim/lua/plugins/interface/harpoon.lua
+++ config/nvim/lua/plugins/interface/harpoon.lua
@@ -1,26 +1,25 @@
 return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
-  -- event = 'VeryLazy',
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
   opts = function()
     local harpoon = require('harpoon')
     local set = function(lhs, rhs, desc)
-      vim.keymap.set('n', '<leader>h' .. lhs, rhs, { silent = true, desc = desc })
+      vim.keymap.set('n', '<leader>h' .. lhs, rhs, { silent = true, desc = 'Harpoon: ' .. desc })
     end
    -- stylua: ignore start
-    set('a', function() harpoon:list():append() end, 'Harpoon this file')
-    set('l', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, 'List Harpoons')
-    set('n', function() harpoon:list():next() end, 'Next Harpoon')
-    set('p', function() harpoon:list():prev() end, 'Prev Harpoon')
+    set('a', function() harpoon:list():append() end, 'Add')
+    set('l', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, 'List')
+    set('n', function() harpoon:list():next() end, 'Next')
+    set('p', function() harpoon:list():prev() end, 'Previous')
     -- stylua: ignore end
     for i = 1, 9 do
       local si = tostring(i)
       set(si, function()
         harpoon:list():select(i)
-      end, 'Go to Harpoon #' .. si)
+      end, 'Go to #' .. si)
     end
   end,
 }

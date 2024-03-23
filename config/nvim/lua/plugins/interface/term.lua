a5887a232ed6fc2e4542edf64b4dc9943c715fb3
Arsen Nuramatov <anuramat@pm.me>
2024-03-23 21:43:36 +0500

    auto

--- config/nvim/lua/plugins/interface/term.lua
+++ config/nvim/lua/plugins/interface/term.lua
@@ -3,11 +3,17 @@ return {
   version = '*',
   opts = {
     start_in_insert = false,
-    -- float_opts = {
-    --   border = vim.g.border,
-    -- },
+    float_opts = {
+      --   border = 'solid',
+      winblend = vim.o.winblend,
+    },
   },
-  highlights = {},
+  -- highlights = {
+  --   NormalFloat = {
+  --     guibg = '#123456',
+  --     guifg = '#000000',
+  --   },
+  -- },
   -- keys = { { '<leader>t', '<cmd>ToggleTerm<cr>' } },
   keys = { { '<leader>t', '<cmd>ToggleTerm direction=float<cr>' } },
 }

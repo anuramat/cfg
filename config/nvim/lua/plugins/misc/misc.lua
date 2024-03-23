ef4e5c140d856d1eaf3e58c181fdd826fb8fa03c
Arsen Nuramatov <anuramat@pm.me>
2024-03-24 01:55:12 +0500

    auto

--- config/nvim/lua/plugins/misc/misc.lua
+++ config/nvim/lua/plugins/misc/misc.lua
@@ -7,26 +7,6 @@ specs.sleuth = {
   event = { 'BufReadPre', 'BufNewFile' },
 }
 
--- Align text interactively
--- See also:
--- junegunn/vim-easy-align
--- godlygeek/tabular
--- tommcdo/vim-lion
--- Vonr/align.nvim
-specs.align = {
-  'echasnovski/mini.align',
-  opts = {
-    mappings = {
-      start = '<leader>a',
-      start_with_preview = '<leader>A',
-    },
-  },
-  keys = {
-    { mode = { 'x', 'n' }, '<leader>a', desc = 'Align' },
-    { mode = { 'x', 'n' }, '<leader>A', desc = 'Interactive align' },
-  },
-}
-
 -- Highlight trailing space
 specs.trailspace = {
   'echasnovski/mini.trailspace',

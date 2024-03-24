f7ebdcb58b5222ad23819d2ce03674a704d92537
Arsen Nuramatov <anuramat@pm.me>
2024-03-24 06:41:53 +0500

    auto

--- config/nvim/lua/plugins/interface/fugitive.lua
+++ config/nvim/lua/plugins/interface/fugitive.lua
@@ -1,6 +1,8 @@
 -- https://github.com/tanvirtin/vgit.nvim
 -- https://github.com/NeogitOrg/neogit
-return {
-  'tpope/vim-fugitive',
-  lazy = false, -- so that merge tool works
-}
+-- return {
+--   'tpope/vim-fugitive',
+--   lazy = false, -- so that merge tool works
+-- }
+
+return { 'NeogitOrg/neogit', lazy = false, opts = {} }
--- config/nvim/lua/plugins/ui/illuminate.lua
+++ config/nvim/lua/plugins/ui/illuminate.lua
@@ -1,1 +1,15 @@
-return { 'RRethy/vim-illuminate', event = 'VeryLazy' }
+return {
+  'RRethy/vim-illuminate',
+  event = 'VeryLazy',
+  config = function()
+    require('illuminate').configure({
+      filetypes_denylist = { -- TODO make a vim.g.nonfiles
+        'NeogitStatus',
+        'oil',
+        'lazy',
+        'lspinfo',
+        'null-ls-info',
+      },
+    })
+  end,
+}

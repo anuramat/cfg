3040b588985cb39b0da543e44e1eab097335ef28
Arsen Nuramatov <anuramat@pm.me>
2024-03-25 16:04:43 +0500

    auto

--- config/nvim/lua/plugins/languages/latex.lua
+++ config/nvim/lua/plugins/languages/latex.lua
@@ -9,13 +9,4 @@ return {
     ]])
     end,
   },
-  {
-    'jbyuki/nabla.nvim',
-    ft = { 'latex', 'markdown' },
-    init = function() -- not sure if this will even work
-      vim.cmd([[
-        nnoremap <leader>p :lua require("nabla").popup()<CR> " Customize with popup({border = ...})  : `single` (default), `double`, `rounded`
-      ]])
-    end,
-  },
 }

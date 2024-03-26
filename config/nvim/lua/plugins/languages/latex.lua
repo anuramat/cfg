a6fd411a37344775609bac7eb598c5cfa4149ee1
Arsen Nuramatov <anuramat@pm.me>
2024-03-27 02:53:53 +0500

    auto

--- config/nvim/lua/plugins/languages/latex.lua
+++ config/nvim/lua/plugins/languages/latex.lua
@@ -29,6 +29,21 @@ return {
       let g:tex_flavor='latex'
       let g:tex_conceal='abdmg'
       let g:vimtex_quickfix_mode=0 " do not open qf automatically
+      let g:vimtex_syntax_conceal = {
+            \ 'accents': 1,
+            \ 'ligatures': 1,
+            \ 'cites': 1,
+            \ 'fancy': 1,
+            \ 'spacing': 1,
+            \ 'greek': 1,
+            \ 'math_bounds': 1,
+            \ 'math_delimiters': 1,
+            \ 'math_fracs': 1,
+            \ 'math_super_sub': 1,
+            \ 'math_symbols': 1,
+            \ 'sections': 0,
+            \ 'styles': 1,
+            \}
       " let g:vimtex_syntax_conceal=
       " g:vimtex_syntax_conceal_disable
       """ a = accents/ligatures
---
+++ config/nvim/lua/plugins/misc/devdocs.lua
@@ -0,0 +1,10 @@
+return {
+  'luckasRanarison/nvim-devdocs',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = {},
+}

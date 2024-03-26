749c8510fd91c129d0aab2e94ef0206a289a6d3d
Arsen Nuramatov <anuramat@pm.me>
2024-03-26 08:16:20 +0500

    auto

--- config/nvim/lua/adhoc/gramota.lua
+++ config/nvim/lua/adhoc/gramota.lua
@@ -1,9 +1,22 @@
 local Job = require('plenary.job')
 
 local result_block_type_name = 'result'
-local start_pattern = '^```%w+'
-local end_pattern = '^```%s*$'
+local border_pattern = '^```%w*%s*$'
 local placeholder_format = 'generating %s output [%s], do not edit: %s' -- one line only
+
+---@class block
+---@field start integer Line number with the opening triple backtick
+---@field finish integer Line number with the closing triple backtick
+---@field language string Language of the code block
+---@field code string Code inside the block
+
+---@class border
+---@field position integer Line number
+---@field contents string Text in the line
+
+---@class program
+---@field full_path? string Full path to a binary
+---@field name? string Name of the binary in $PATH
 
 --- Generates an v4 UUID (stolen from internets)
 ---@return string uuid
@@ -21,15 +34,16 @@ end
 ---@param lhs string
 ---@param rhs string
 local function fuck(buffer_id, lhs, rhs)
+  -- WARNING this function should actually be atomic
+  -- TODO figure out how to :(
   local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
   -- build the result block
   local rhs_table = vim.split(rhs, '\n', { trimempty = true })
-  table.insert(rhs_table, '```') -- TODO remove dashes
-  table.insert(rhs_table, 1, '```' .. result_block_type_name) -- TODO remove dashes
+  table.insert(rhs_table, '```')
+  table.insert(rhs_table, 1, '```' .. result_block_type_name)
   -- insert
   for i, line in ipairs(lines) do
     if line == lhs then
-      vim.print(rhs_table)
       vim.api.nvim_buf_set_lines(buffer_id, i - 1, i, false, rhs_table)
       break
     end
@@ -40,7 +54,7 @@ end
 ---@param buffer_id integer
 ---@param placeholder string Placeholder to be replaced (verbatim)
 ---@param code string[]
----@param program table Interpreter for the code
+---@param program program Interpreter for the code
 --- TODO generalize for non filterable programs or make it a wrapper idk (use utils.merge?)
 local function _insert_code(buffer_id, placeholder, code, program)
   local env = {}
@@ -51,12 +65,12 @@ local function _insert_code(buffer_id, placeholder, code, program)
   else
     command = program.full_path
   end
+  assert(command ~= nil, 'empty command')
+
   Job:new({
     command = command,
     args = {},
     env = env,
-    -- WARNING this function should actually be atomic
-    -- TODO figure out how to :(
     on_stdout = function(_, data)
       vim.schedule(function()
         fuck(buffer_id, placeholder, data)
@@ -68,6 +82,7 @@ end
 
 local function insert_code(buffer_id, placeholder, lang, code)
   if lang == 'python' then
+    vim.print('test')
     _insert_code(buffer_id, placeholder, code, { name = 'python' })
   end
 end
@@ -82,73 +97,72 @@ local function wipe_placeholders(buffer)
   -- vim.cmd('normal ``zz')
 end
 
---- Wipes result blocks from the buffer
----@param buffer integer Buffer id
-local function wipe_results(buffer)
-  -- TODO implement
-end
-
---- Finds the first markdown code block starting from given offset
----@param buffer_lines string[]
----@param offset integer
----@return string, integer, integer, boolean
-local function find_block(buffer_lines, offset)
-  local start ---@type integer
-  local lang ---@type string
-  local finish ---@type integer
-  local block_found ---@type boolean
-  for i = offset + 1, #buffer_lines do
-    local line = buffer_lines[i]
-    if not lang then
-      local start_match = string.match(line, start_pattern)
-      if start_match then
-        start = i
-        lang = string.sub(start_match, 4)
-      end
-    end
-    if lang and string.find(line, end_pattern) then
-      finish = i
-      block_found = true
-      break
+--- Finds all markdown code blocks
+--- @param lines string[]
+---@return block[]
+local function find_blocks(lines)
+  local borders = {} ---@type border[]
+  for i = 1, #lines do
+    local line = lines[i]
+    if string.match(line, border_pattern) then
+      table.insert(borders, {
+        position = i,
+        contents = line,
+      })
     end
   end
-  return lang, start, finish, block_found
+  assert(#borders % 2 == 0, 'invalid code blocks!')
+  local blocks = {} ---@type block[]
+  for i = 1, #borders / 2 do
+    local start = borders[i * 2 - 1]
+    local finish = borders[i * 2]
+    local code_lines = vim.list_slice(lines, start.position + 1, finish.position - 1)
+    -- local code = vim.iter(code_lines):join('\n')
+    local language = string.gsub(start.contents, '%W*', '')
+    if language then
+      table.insert(blocks, {
+        start = start.position,
+        finish = finish.position,
+        code = code_lines,
+        language = language,
+      })
+    end
+  end
+  return blocks
+end
+
+--- Wipes result blocks from the buffer
+---@param buffer_id integer Buffer id
+local function wipe_results(buffer_id)
+  local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
+  local blocks = find_blocks(lines)
+  for i = #blocks, 1, -1 do
+    local block = blocks[i]
+    if block.language == result_block_type_name then
+      vim.api.nvim_buf_set_lines(buffer_id, block.start, block.finish + 1, false, {})
+    end
+  end
 end
 
 vim.api.nvim_create_user_command('Exec', function()
-  local buffer = vim.api.nvim_get_current_buf()
-  wipe_placeholders(buffer)
-  wipe_results(buffer)
-  local offset = 0 -- last code block position on j-th iteration
-  local buffer_lines = vim.api.nvim_buf_get_lines(buffer, 0, vim.api.nvim_buf_line_count(buffer), false)
-  local j = -1
-  local replacers = {}
-  while true do
-    j = j + 1
-    vim.print(j)
-
-    -- find code block
-    local lang, start, finish, found = find_block(buffer_lines, offset)
-    offset = finish
-    if not found then
-      break
-    end
-    vim.print(lang, start, finish)
-
-    -- extract the code block
-    local code_lines = vim.list_slice(buffer_lines, start + 1, finish - 1)
-    local code = vim.iter(code_lines):join('\n')
-    vim.print(code)
-
-    -- -- insert a placeholder that will be replaced with the code output
-    local placeholder = string.format(placeholder_format, lang, tostring(j), make_uuid())
-    vim.api.nvim_buf_set_lines(buffer, finish + j, finish + j, false, { placeholder })
-
-    table.insert(replacers, function()
-      insert_code(buffer, placeholder, lang, code)
+  local buffer_id = vim.api.nvim_get_current_buf()
+  wipe_placeholders(buffer_id)
+  wipe_results(buffer_id)
+  local job_list = {}
+  local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
+  -- iterate over codeblocks starting from the end
+  local blocks = find_blocks(lines)
+  for i = #blocks, 1, -1 do
+    local block = blocks[i]
+    -- add placeholder after block
+    local placeholder = string.format(placeholder_format, block.language, tostring(i), make_uuid())
+    vim.api.nvim_buf_set_lines(buffer_id, block.finish, block.finish, false, { placeholder })
+    -- defer the code runner
+    table.insert(job_list, function()
+      insert_code(buffer_id, placeholder, block.language, block.code)
     end)
   end
-  for _, f in ipairs(replacers) do
+  for _, f in ipairs(job_list) do
     f()
   end
 end, {})

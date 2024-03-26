2c6d04932c48f50b82ef21eff93d5169ff9ae15a
Arsen Nuramatov <anuramat@pm.me>
2024-03-26 12:21:00 +0500

    auto

--- config/nvim/lua/adhoc/gramota.lua
+++ config/nvim/lua/adhoc/gramota.lua
@@ -1,18 +1,19 @@
 local Job = require('plenary.job')
 
-local result_block_type_name = 'result'
+local output_language = 'result'
 local border_pattern = '^```%w*%s*$'
 local placeholder_format = 'generating %s output [%s], do not edit: %s' -- one line only
+local output_header = ':::OUTPUT:::'
 
 ---@class block
 ---@field start integer Line number with the opening triple backtick
 ---@field finish integer Line number with the closing triple backtick
----@field language string Language of the code block
----@field code string[] Code inside the block
+---@field language string Input language
+---@field input string[] Input inside the block
 
 ---@class border
 ---@field position integer Line number
----@field contents string Text in the line
+---@field language string
 
 ---@class program
 ---@field full_path? string Full path to a binary
@@ -29,7 +30,15 @@ local function make_uuid()
   return uuid
 end
 
---- Replaces a placeholder, wrapping into a result block
+--- Extracts language from a ``` line
+---@param line string
+---@return string language
+local function get_language(line)
+  local match, _ = string.gsub(line, '%W*', '')
+  return match
+end
+
+--- Replaces a placeholder, wrapping into a output block
 ---@param buffer_id integer
 ---@param lhs string
 ---@param rhs string[]
@@ -38,10 +47,12 @@ local function fuck(buffer_id, lhs, rhs)
   -- although it works for now
   -- TODO figure out how to :(
   local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
-  -- build the result block
+  -- build the output block
   table.insert(rhs, '```')
-  table.insert(rhs, 1, ':::OUTPUT:::')
-  table.insert(rhs, 1, '```' .. result_block_type_name)
+  if output_header then
+    table.insert(rhs, 1, output_header)
+  end
+  table.insert(rhs, 1, '```' .. output_language)
   -- insert
   for i, line in ipairs(lines) do
     if line == lhs then
@@ -52,13 +63,13 @@ local function fuck(buffer_id, lhs, rhs)
 end
 
 --- Replaces placeholder with output of a command
----@param program program Interpreter for the code
+---@param program program Interpreter for the input
 ---@return function
 local function stdin_interpreter(program)
   ---@param buffer_id integer
   ---@param placeholder string Placeholder to be replaced (verbatim)
-  ---@param code string[]
-  return function(buffer_id, placeholder, code)
+  ---@param input string[]
+  return function(buffer_id, placeholder, input)
     local env = {}
     local command
     if not program.full_path then
@@ -73,13 +84,14 @@ local function stdin_interpreter(program)
       command = command,
       args = {},
       env = env,
+      ---@diagnostic disable-next-line: unused-local
       on_exit = function(j, return_val)
         vim.schedule(function()
           -- vim.print('return val', return_val) -- XXX return code?
           fuck(buffer_id, placeholder, j:result())
         end)
       end,
-      writer = code,
+      writer = input,
     }):start()
   end
 end
@@ -89,41 +101,45 @@ local languages = {
   lua = stdin_interpreter({ name = 'lua' }),
 }
 
---- Replaces a placeholder with the results of running code through an interpreter
+--- Replaces a placeholder with the interpreter output
 ---@param buffer_id integer
 ---@param placeholder string
 ---@param language string
----@param code string[]
-local function insert_code(buffer_id, placeholder, language, code)
-  languages[language](buffer_id, placeholder, code)
+---@param input string[]
+local function insert_output(buffer_id, placeholder, language, input)
+  languages[language](buffer_id, placeholder, input)
 end
 
---- Finds all markdown code blocks
+--- Finds all markdown input blocks in range
 --- @param lines string[]
+--- @param search_start? integer first line, 1 by default
+--- @param max_blocks? integer
 ---@return block[]
-local function find_blocks(lines)
+local function find_blocks(lines, search_start, max_blocks)
   local borders = {} ---@type border[]
-  for i = 1, #lines do
+  for i = search_start, #lines do
     local line = lines[i]
     if string.match(line, border_pattern) then
       table.insert(borders, {
         position = i,
-        contents = line,
+        language = get_language(line),
       })
     end
+    if max_blocks and #borders == 2 * max_blocks then
+      break
+    end
   end
-  assert(#borders % 2 == 0, 'invalid code blocks!')
+  assert(#borders % 2 == 0, 'invalid blocks')
   local blocks = {} ---@type block[]
   for i = 1, #borders / 2 do
     local start = borders[i * 2 - 1]
     local finish = borders[i * 2]
-    local language = string.gsub(start.contents, '%W*', '')
-    if language then
+    if start.language then
       table.insert(blocks, {
         start = start.position,
         finish = finish.position,
-        code = vim.list_slice(lines, start.position + 1, finish.position - 1),
-        language = language,
+        input = vim.list_slice(lines, start.position + 1, finish.position - 1),
+        language = start.language,
       })
     end
   end
@@ -136,9 +152,18 @@ local function wipe_placeholders(buffer_id)
   local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
   for i = #lines, 1, -1 do
     local line = lines[i]
-    if string.match(line, string.format(placeholder_format, '%w+', '%d+', '[%x-]*')) then
+    if string.match(line, string.format(placeholder_format, '%w+', '%S+', '[%x-]*')) then
       vim.api.nvim_buf_set_lines(buffer_id, i - 1, i - 1, false, {})
     end
+  end
+end
+
+--- Removes a block, if it's an output block
+---@param buffer_id integer
+---@param block block
+local function wipe_block(buffer_id, block)
+  if block.language == output_language then
+    vim.api.nvim_buf_set_lines(buffer_id, block.start - 1, block.finish, false, {})
   end
 end
 
@@ -148,11 +173,18 @@ local function wipe_results(buffer_id)
   local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
   local blocks = find_blocks(lines)
   for i = #blocks, 1, -1 do
-    local block = blocks[i]
-    if block.language == result_block_type_name then
-      vim.api.nvim_buf_set_lines(buffer_id, block.start - 1, block.finish, false, {})
-    end
+    wipe_block(buffer_id, blocks[i])
   end
+end
+
+--- inserts a placeholder after the block, returning the text of the placeholder
+---@param buffer_id integer
+---@param block block Input block, for which the placeholder is generated
+---@param identifier string Output block number/name
+local function make_placeholder(buffer_id, block, identifier)
+  local placeholder = string.format(placeholder_format, block.language, identifier, make_uuid())
+  vim.api.nvim_buf_set_lines(buffer_id, block.finish, block.finish, false, { placeholder })
+  return placeholder
 end
 
 --- Executes all blocks in the buffer
@@ -162,16 +194,14 @@ local function exec_all()
   wipe_results(buffer_id)
   local job_list = {}
   local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
-  -- iterate over codeblocks starting from the end
+  -- iterate over blocks starting from the end
   local blocks = find_blocks(lines)
   for i = #blocks, 1, -1 do
     local block = blocks[i]
-    -- add placeholder after block
-    local placeholder = string.format(placeholder_format, block.language, tostring(i), make_uuid())
-    vim.api.nvim_buf_set_lines(buffer_id, block.finish, block.finish, false, { placeholder })
-    -- defer the code runner
+    local placeholder = make_placeholder(buffer_id, block, tostring(i))
+    -- defer the wrapped interpreter
     table.insert(job_list, function()
-      insert_code(buffer_id, placeholder, block.language, block.code)
+      insert_output(buffer_id, placeholder, block.language, block.input)
     end)
   end
   for _, f in ipairs(job_list) do
@@ -179,36 +209,59 @@ local function exec_all()
   end
 end
 
---- Executes a block that contains the specified line
----@param line integer The line in the block
-local function exec_one(buffer_id, position)
-  local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
-  local language
-  local start
-  local finish
-  for i = position, 1, -1 do
-    local line = lines[i]
+--- Seeks the first border going up
+---@param lines string[]
+---@param start integer Starting line (inclusive)
+---@return border? border
+local function seek_up(lines, start)
+  for position = start, 1, -1 do
+    local line = lines[position]
     if string.match(line, border_pattern) then
-      language = string.gsub(line, '%W*', '')
-      start = i
-      break
+      return {
+        position = position,
+        language = get_language(line),
+      }
     end
   end
-  assert(language ~= '', 'no language specified for the block')
-  for i = position, #lines do
-    local line = lines[i]
-    if string.match(line, border_pattern) then
-      finish = i
-    end
-  end
-  vim.print(start, finish) -- XXX
-  -- TODO find the next block and delete it if it's a result block
-  -- ACTUALLY no, I want this to work from any line of either result or source blocks
+  return nil
 end
 
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mappings ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ --
+--- Executes a single block (cursor should be between the start of the input block
+--- and the end of the output block, inclusive on both sides).
+--- Source block and output block are treated as one if there is no non-whitespace
+--- characters between them.
+---@param buffer_id integer
+---@param position integer Row number of the cursor
+local function exec_one(buffer_id, position)
+  local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
+  -- step 1 - upstroke
+  -- find the ```language line
+  local search_start = position
+  for _ = 1, 3 do
+    local border = seek_up(lines, search_start)
+    if not border then
+      -- no borders above == no block to run
+      return
+    end
+    if border.language ~= '' and border.language ~= output_language then
+      -- we found the start, proceed to next step
+      break
+    end
+    search_start = border.position - 1
+  end
+  -- step 2 - downstroke
+  -- remove the output block
+  local blocks = find_blocks(lines, search_start, 2)
+  wipe_block(buffer_id, blocks[2])
+  -- make placeholder
+  local placeholder = make_placeholder(buffer_id, blocks[1], 'solo') -- TODO check the pattern matching on placeholders
+  -- run the interpreter
+  insert_output(buffer_id, placeholder, blocks[1].language, blocks[1].input)
+end
 
-vim.api.nvim_create_user_command('GramotaWipe', function()
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Commands ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ --
+
+vim.api.nvim_create_user_command('GramotaWipeAll', function()
   wipe_results(0)
   wipe_placeholders(0)
 end, {})
@@ -219,6 +272,8 @@ vim.api.nvim_create_user_command('GramotaExecOne', function()
   local buffer_id = vim.api.nvim_get_current_buf()
   exec_one(buffer_id, row)
 end, {})
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Mappings ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ --
 
 vim.cmd('map <leader>Y <cmd>GramotaExecAll<cr>')
 vim.cmd('map <leader>y <cmd>GramotaExecOne<cr>')

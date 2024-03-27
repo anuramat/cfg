eed0827eded36f70e283c8fec4c5c290f40dd9c7
Arsen Nuramatov <anuramat@pm.me>
2024-03-27 05:03:12 +0500

    auto

--- config/nvim/lua/plugins/ui/dashboard.lua
+++ config/nvim/lua/plugins/ui/dashboard.lua
@@ -43,7 +43,7 @@ return {
     local function info()
       local datetime = os.date(' %d-%m-%Y   %H:%M:%S')
       local version = vim.version()
-      local nvim_version_info = 'v' .. version.major .. '.' .. version.minor .. '.' .. version.patch
+      local nvim_version_info = ' ' .. version.major .. '.' .. version.minor .. '.' .. version.patch
       return datetime .. '   ' .. nvim_version_info
     end
     local layout = {

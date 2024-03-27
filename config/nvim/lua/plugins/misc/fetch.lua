dfc3d2216ae9d52734931f36ad749cdba7132e6a
Arsen Nuramatov <anuramat@pm.me>
2024-03-27 15:18:21 +0500

    auto

--- config/nvim/lua/plugins/misc/fetch.lua
+++ config/nvim/lua/plugins/misc/fetch.lua
@@ -1,6 +1,7 @@
 -- features:
 -- - `nvim $filename:$line` opens the file on the line $line
 -- - `nvim $filename +/pattern` searches on open
+-- TODO check if this breaks gf gF
 return {
   lazy = false,
   'wsdjeg/vim-fetch',

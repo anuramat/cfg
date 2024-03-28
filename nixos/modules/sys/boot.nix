724d25189761098284e859fc9e6390d6315b9885
Arsen Nuramatov <anuramat@pm.me>
2024-03-28 20:42:56 +0500

    auto

--- nixos/modules/sys/boot.nix
+++ nixos/modules/sys/boot.nix
@@ -1,12 +1,18 @@
 _: {
   boot = {
+    # silent boot
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = ["quiet" "udev.log_level=3"];
+    # boot splashscreen
+    plymouth = {
+      enable = true;
+      theme = "breeze"; # nixos logo
+    };
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
     kernelModules = ["v4l2loopback"]; # virtual webcam
-    plymouth = {
-      enable = true;
-    };
   };
 }

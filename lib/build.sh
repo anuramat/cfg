5951fbeef04b68c137bd6be47adf9ed72ca98f40
Arsen Nuramatov <anuramat@pm.me>
2024-03-30 21:37:46 +0500

    auto

--- lib/build.sh
+++ lib/build.sh
@@ -21,3 +21,6 @@ sudo nixos-rebuild switch
 
 # clear tofi .desktop cache
 [ -f "${tofi_drun_cache}" ] && rm "${tofi_drun_cache}" || true
+
+# create XDG user directories
+xdg-user-dirs-update --force

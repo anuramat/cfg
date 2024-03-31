57f10aa1e9a6a0f21b66b5bc3db62b99dfe1485f
Arsen Nuramatov <anuramat@pm.me>
2024-03-31 17:15:24 +0500

    auto

--- nixos/modules/software/terminal_env/main.nix
+++ nixos/modules/software/terminal_env/main.nix
@@ -3,11 +3,11 @@
   unstable,
   ...
 }: {
-  # nixpkgs.overlays = [
-  #   (import (builtins.fetchTarball {
-  #     url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
-  #   }))
-  # ];
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+    }))
+  ];
   environment.systemPackages = with pkgs; [
     # Code editors
     unstable.neovim

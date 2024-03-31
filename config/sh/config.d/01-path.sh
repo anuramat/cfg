2e1f2df7b674ed8cd9314c7e7682088c019263b4
Arsen Nuramatov <anuramat@pm.me>
2024-03-31 11:40:14 +0500

    auto

--- config/sh/config.d/01-path.sh
+++ config/sh/config.d/01-path.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
-export PATH="${PATH:+$PATH:}$GOPATH/bin"
-export PATH="$HOME/.ghcup/bin${PATH:+:$PATH}"
-export PATH="$HOME/.local/bin${PATH:+:$PATH}"
+export __custom_paths_exported=1
+[ "$__custom_paths_exported" ] || {
+	export PATH="${PATH:+$PATH:}$GOPATH/bin"
+	export PATH="$HOME/.ghcup/bin${PATH:+:$PATH}"
+	export PATH="$HOME/.local/bin${PATH:+:$PATH}"
+}

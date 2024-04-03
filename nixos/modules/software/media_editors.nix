87bc3f9b54b511cb087973542d0732020f7c18c9
Arsen Nuramatov <anuramat@pm.me>
2024-04-04 04:14:48 +0500

    auto

--- nixos/modules/software/media_editors.nix
+++ nixos/modules/software/media_editors.nix
@@ -1,14 +1,14 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    # # Graphics
+    ### Graphics
     # krita # raster graphics, digital art
-    # inkscape-with-extensions # vector graphics
+    inkscape-with-extensions # vector graphics
     gimp-with-plugins # raster graphics
     imagemagickBig # CLI image manipulation
     libwebp # tools for WebP image format
     exiftool # read/write EXIF metadata
 
-    # # Video
+    ### Video
     # davinci-resolve # video editor etc
     handbrake # ghb - GUI for video converting
     ffmpeg # CLI multimedia processing

bb4848fb5ebecb32b8f8213b8889361d9dd278a5
Arsen Nuramatov <anuramat@pm.me>
2024-04-05 03:24:38 +0500

    auto

--- nixos/modules/system/services.nix
+++ nixos/modules/system/services.nix
@@ -1,18 +1,16 @@
 {user, ...}: {
-  # TODO restic backups
-
   programs.seahorse.enable = true; # gnome keyring frontend
 
   virtualisation.docker.enable = true;
   services = {
     gnome.gnome-keyring.enable = true; # security credential storage, exposed over dbus
-    syncthing = {
-      enable = true;
-      user = user.username;
-      dataDir = "/home/${user.username}"; # parent directory for synchronised folders
-      configDir = "/home/${user.username}/.config/syncthing"; # keys and settings
-      databaseDir = "/home/${user.username}/.local/share/syncthing"; # database and logs
-      # TODO somehow use XDG variables
-    };
+    # syncthing = {
+    #   enable = true;
+    #   user = user.username;
+    #   dataDir = "/home/${user.username}/Syncthing"; # parent directory for synchronised folders
+    #   configDir = "/home/${user.username}/.config/syncthing"; # keys and settings
+    #   databaseDir = "/home/${user.username}/.local/share/syncthing"; # database and logs
+    #   # if unccomented, somehow use XDG variables
+    # };
   };
 }

1c602331cb817fd252ba66c6528b3d52825ed9a9
Arsen Nuramatov <anuramat@pm.me>
2024-04-05 22:56:53 +0500

    auto

--- nixos/modules/software/desktop_env/theme.nix
+++ nixos/modules/software/desktop_env/theme.nix
@@ -2,17 +2,19 @@
   # TODO
   environment.systemPackages = with pkgs; [
     glib # gsettings (gtk etc)
-    qt5ct # qt5 gui settings
-    qt6ct # qt6 gui settings
-    adwaita-qt
     dracula-theme
     dracula-icon-theme
     hackneyed # windows style cursor
     gnome3.adwaita-icon-theme
+
+    adwaita-qt # qt5
+    adwaita-qt6
+    qt5ct # qt5 gui settings
+    qt6ct # qt6 gui settings
   ];
   qt = {
     enable = true;
-    platformTheme = "qt5ct";
+    platformTheme = "qt5ct"; # actually uses both qt5ct and qt6ct
   };
   # home-manager.users.myUser = {
   #   home.pointerCursor = {

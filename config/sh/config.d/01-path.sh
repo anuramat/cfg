bff1b268fe9a04127e0f8000d2a860204c47d6d3
Arsen Nuramatov <anuramat@pm.me>
2024-04-10 08:28:17 +0500

    auto

--- config/sh/config.d/01-path.sh
+++ config/sh/config.d/01-path.sh
@@ -2,6 +2,6 @@
 export __custom_paths_exported=1
 [ "$__custom_paths_exported" ] || {
 	export PATH="${PATH:+$PATH:}$GOPATH/bin"
-	export PATH="$HOME/.ghcup/bin${PATH:+:$PATH}"
 	export PATH="$HOME/.local/bin${PATH:+:$PATH}"
+	export PATH="${PATH:+$PATH:}$XDG_CONFIG_HOME/emacs/bin"
 }

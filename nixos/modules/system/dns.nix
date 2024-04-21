41d736d6925d85d7c8dc8ae5e229cc53d7250eba
Arsen Nuramatov <anuramat@pm.me>
2024-04-21 18:16:24 +0200

    auto

--- nixos/modules/system/dns.nix
+++ nixos/modules/system/dns.nix
@@ -1,15 +1,14 @@
 _: {
-  # # Doesn't work very well with captive portals
-  # # TODO why set nameservers twice?
-  # networking = {
-  #   nameservers = ["1.1.1.1" "1.0.0.1"];
-  # };
-  # # uses DNSSEC and DNSoverTLS, might break on a different ns
-  # services.resolved = {
-  #   enable = true;
-  #   dnssec = "true";
-  #   dnsovertls = "true";
-  #   domains = ["~."]; # TODO what does this do?
-  #   fallbackDns = ["1.1.1.1" "1.0.0.1"];
-  # };
+  # TODO why set nameservers twice?
+  networking = {
+    nameservers = ["1.1.1.1" "1.0.0.1"];
+  };
+  # uses DNSSEC and DNSoverTLS, might break on a different ns
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    dnsovertls = "true";
+    domains = ["~."]; # TODO what does this do?
+    fallbackDns = ["1.1.1.1" "1.0.0.1"];
+  };
 }

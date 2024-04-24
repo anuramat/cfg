6e0e6e6dbfef739906da73dc24f2376d6110a36c
Arsen Nuramatov <anuramat@pm.me>
2024-04-24 23:56:14 +0200

    auto

--- config/nvim/lua/plugins/misc/tablemode.lua
+++ config/nvim/lua/plugins/misc/tablemode.lua
@@ -1,10 +1,10 @@
 return {
   'dhruvasagar/vim-table-mode',
-  config = function()
+  init = function()
     vim.g.table_mode_disable_mappings = 1
-    vim.cmd([[
-      autocmd FileType markdown nnoremap <leader><leader> <cmd>TableModeRealign<cr>
-    ]])
+    vim.g.table_mode_disable_tableize_mappings = 1
+    vim.keymap.set('n', '<leader>T', '<cmd>TableModeToggle<cr>', { silent = true, desc = 'Table Mode: Toggle' })
+    vim.keymap.set('n', '<leader>t', '<cmd>TableModeRealign<cr>', { silent = true, desc = 'Table Mode: Realign once' })
   end,
   ft = 'markdown',
 }

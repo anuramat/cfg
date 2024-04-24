5439961712c876d1a7aa830feea8d27723858572
Arsen Nuramatov <anuramat@pm.me>
2024-04-24 23:21:39 +0200

    auto

--- config/nvim/lua/plugins/misc/tablemode.lua
+++ config/nvim/lua/plugins/misc/tablemode.lua
@@ -2,6 +2,9 @@ return {
   'dhruvasagar/vim-table-mode',
   config = function()
     vim.g.table_mode_disable_mappings = 1
+    vim.cmd([[
+      autocmd FileType markdown nnoremap <leader><leader> <cmd>TableModeRealign<cr>
+    ]])
   end,
   ft = 'markdown',
 }

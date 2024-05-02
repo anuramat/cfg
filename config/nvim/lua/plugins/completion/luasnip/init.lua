7e79580f4c91f99a9829184b63c9587419efd963
Arsen Nuramatov <anuramat@pm.me>
2024-05-02 04:36:00 +0200

    auto

--- config/nvim/lua/plugins/completion/luasnip/init.lua
+++ config/nvim/lua/plugins/completion/luasnip/init.lua
@@ -2,7 +2,7 @@ return {
   'L3MON4D3/LuaSnip',
   version = 'v2.*',
   dependencies = {
-    'rafamadriz/friendly-snippets',
+    'anuramat/friendly-snippets',
     config = function()
       require('luasnip.loaders.from_vscode').lazy_load()
     end,

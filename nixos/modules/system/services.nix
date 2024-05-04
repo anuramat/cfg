a334f6737551e0c29d4aa8f08410989aa229e07d
Arsen Nuramatov <anuramat@pm.me>
2024-05-05 01:22:53 +0200

    auto

--- nixos/modules/system/services.nix
+++ nixos/modules/system/services.nix
@@ -1,8 +1,17 @@
 _: {
   programs.seahorse.enable = true; # gnome keyring frontend
 
-  # virtualisation.docker.enable = true;
-  virtualisation.podman.enable = true;
+  virtualisation = {
+    # common container config files in /etc/containers
+    virtualisation.containers.enable = true;
+    podman = {
+      enable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
   services = {
     gnome.gnome-keyring.enable = true; # security credential storage, exposed over dbus
     # syncthing = {

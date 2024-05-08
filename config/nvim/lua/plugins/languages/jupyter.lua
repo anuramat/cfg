a2330b7cb112485fd68f00dfc0197d25cfe6c059
Arsen Nuramatov <anuramat@pm.me>
2024-05-08 04:29:50 +0200

    auto

--- config/nvim/lua/plugins/languages/jupyter.lua
+++ config/nvim/lua/plugins/languages/jupyter.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    'GCBallesteros/jupytext.nvim',
-    opts = {},
-    lazy = false,
-  },
   -- {
-  --   'goerz/jupytext.vim',
+  --   'GCBallesteros/jupytext.nvim',
+  --   opts = {},
   --   lazy = false,
-  --   init = function()
-  --     vim.g.jupytext_fmt = 'py'
-  --   end,
   -- },
+  {
+    'goerz/jupytext.vim',
+    lazy = false,
+    init = function()
+      vim.g.jupytext_fmt = 'py'
+    end,
+  },
 }

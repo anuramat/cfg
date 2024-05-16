75f0216c725776bf884f7a3864a2412437b8d12b
Arsen Nuramatov <anuramat@pm.me>
2024-05-16 23:14:50 +0200

    auto

--- nixos/user.nix
+++ nixos/user.nix
@@ -2,7 +2,7 @@
 {
   username = "anuramat";
   fullname = "Arsen Nuramatov";
-  hostname = "nixos";
+  hostname = "anuramat-ll7";
   timezone = "Etc/GMT-2"; # inverted offset (posix momento)
   stateVersion = "23.05"; # WARNING DO NOT EDIT
   defaultLocale = "en_US.UTF-8";

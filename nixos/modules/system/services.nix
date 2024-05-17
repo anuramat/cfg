52cd0ea9b78440e573d065f414bd7b492890addc
Arsen Nuramatov <anuramat@pm.me>
2024-05-17 12:33:18 +0200

    auto

--- nixos/modules/system/services.nix
+++ nixos/modules/system/services.nix
@@ -14,13 +14,5 @@ _: {
   };
   services = {
     gnome.gnome-keyring.enable = true; # security credential storage, exposed over dbus
-    # syncthing = {
-    #   enable = true;
-    #   user = user.username;
-    #   dataDir = "/home/${user.username}/Syncthing"; # parent directory for synchronised folders
-    #   configDir = "/home/${user.username}/.config/syncthing"; # keys and settings
-    #   databaseDir = "/home/${user.username}/.local/share/syncthing"; # database and logs
-    #   # if unccomented, somehow use XDG variables
-    # };
   };
 }

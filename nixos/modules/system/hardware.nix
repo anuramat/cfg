a61b8c7613cfe59b7fe0b83378cf51de4fa1dea0
Arsen Nuramatov <anuramat@pm.me>
2024-05-17 16:17:09 +0200

    auto

--- nixos/modules/system/hardware.nix
+++ nixos/modules/system/hardware.nix
@@ -15,12 +15,24 @@
     ];
   };
 
+  # these are stolen from the wiki https://nixos.wiki/wiki/Nvidia
+  # Enable OpenGL TODO why?
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  # Load nvidia driver for Xorg and Wayland TODO why
+  services.xserver.videoDrivers = ["nvidia"];
+
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
+    powerManagement.finegrained = true;
     prime = {
       intelBusId = "PCI:00:02:0";
       nvidiaBusId = "PCI:01:00:0";
     };
+    nvidiaSettings = true;
   };
 }

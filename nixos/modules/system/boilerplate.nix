4815e9eaa72329356e87b3ed19feffa591bc45af
Arsen Nuramatov <anuramat@pm.me>
2024-05-18 17:22:22 +0200

    auto

--- nixos/modules/system/boilerplate.nix
+++ nixos/modules/system/boilerplate.nix
@@ -4,8 +4,16 @@
   documentation.man.generateCaches = true; # apropos
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
-    trusted-users = ["root" user.username];
-  };
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  }; # kinda imperative
   nixpkgs.config = {
     allowUnfree = true;
   };

0d955dc5ab4d1a32a2d06218f3cb8502c4204e6a
Arsen Nuramatov <anuramat@pm.me>
2024-05-28 14:37:01 +0200

    auto

--- nixos/modules/system/dns.nix
+++ nixos/modules/system/dns.nix
@@ -1,14 +1,14 @@
 _: {
-  # # TODO why set nameservers twice?
-  # networking = {
-  #   nameservers = ["1.1.1.1" "1.0.0.1"];
-  # };
-  # # uses DNSSEC and DNSoverTLS, might break on a different ns
-  # services.resolved = {
-  #   enable = true;
-  #   dnssec = "true";
-  #   dnsovertls = "true";
-  #   domains = ["~."]; # TODO what does this do?
-  #   fallbackDns = ["1.1.1.1" "1.0.0.1"];
-  # };
+  # TODO why set nameservers twice?
+  networking = {
+    nameservers = ["1.1.1.1" "1.0.0.1"];
+  };
+  # uses DNSSEC and DNSoverTLS, might break on a different ns
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    dnsovertls = "true";
+    domains = ["~."]; # TODO what does this do?
+    fallbackDns = ["1.1.1.1" "1.0.0.1"];
+  };
 }

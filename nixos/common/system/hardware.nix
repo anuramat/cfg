3e2bb45b8bf0d79aa800897aa257fd3a4c19e156
Arsen Nuramatov <anuramat@pm.me>
2024-06-08 00:36:10 +0200

    auto

--- nixos/common/system/hardware.nix
+++ nixos/common/system/hardware.nix
@@ -8,30 +8,30 @@
   ];
 
   boot = {
-    initrd.kernelModules = ["nvidia"];
+    # initrd.kernelModules = ["nvidia"];
     extraModulePackages = [
       config.boot.kernelPackages.lenovo-legion-module
-      config.boot.kernelPackages.nvidia_x11
+      # config.boot.kernelPackages.nvidia_x11
     ];
   };
 
-  hardware = {
-    nvidia = {
-      modesetting.enable = true;
-      powerManagement.enable = true;
-      powerManagement.finegrained = true;
-      prime = {
-        intelBusId = "PCI:00:02:0";
-        nvidiaBusId = "PCI:01:00:0";
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-      };
-      nvidiaSettings = true;
-    };
-    opengl.enable = true; # just in case, should be enabled by sway module
-  };
+  # hardware = {
+  #   nvidia = {
+  #     modesetting.enable = true;
+  #     powerManagement.enable = true;
+  #     powerManagement.finegrained = true;
+  #     prime = {
+  #       intelBusId = "PCI:00:02:0";
+  #       nvidiaBusId = "PCI:01:00:0";
+  #       offload = {
+  #         enable = true;
+  #         enableOffloadCmd = true;
+  #       };
+  #     };
+  #     nvidiaSettings = true;
+  #   };
+  #   opengl.enable = true; # just in case, should be enabled by sway module
+  # };
 
   # doesn't support ll7g9, keep just in case
   services.hardware.openrgb.enable = true;

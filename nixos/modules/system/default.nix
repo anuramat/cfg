8b9aba935112adcdb5eca0f5ad3d854ddd91e405
Arsen Nuramatov <anuramat@pm.me>
2024-06-07 23:53:49 +0200

    auto

--- nixos/modules/system/default.nix
+++ nixos/modules/system/default.nix
@@ -2,12 +2,10 @@ _: {
   imports = [
     ./boilerplate.nix
     ./boot.nix
-    ./dns.nix
     ./hardware.nix
     ./laptop.nix
     ./multimedia.nix
     ./networking.nix
-    # ./passthrough.nix
     ./peripherals.nix
     ./printers.nix
     ./services.nix

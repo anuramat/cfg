77b6127d05dfb6e72b307340b5b05e0e01237295
Arsen Nuramatov <anuramat@pm.me>
2024-06-16 19:17:50 +0200

    auto

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -40,7 +40,7 @@
           inputs.nixos-hardware.nixosModules.common-cpu-intel
           inputs.nixos-hardware.nixosModules.common-gpu-intel
           # inputs.nixos-hardware.nixosModules.common-gpu-nvidia
-          inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
+          # inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
           inputs.nixos-hardware.nixosModules.common-pc-laptop
           inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
           inputs.nixos-hardware.nixosModules.common-hidpi

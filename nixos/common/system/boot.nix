0b2fa552f3c45b04b01415ea74098ea9e11985b8
Arsen Nuramatov <anuramat@pm.me>
2024-06-17 01:53:26 +0200

    auto

--- nixos/common/system/boot.nix
+++ nixos/common/system/boot.nix
@@ -8,14 +8,5 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelModules = ["v4l2loopback"]; # virtual webcam
-    # TODO ?? no fucking idea what this one does, probably doesn't work without it
-    # TODO the obs guide has something slightly different?? https://nixos.wiki/wiki/OBS_Studio
-    extraModulePackages = with config.boot.kernelPackages; [v4l2loopback.out];
-    extraModprobeConfig = ''
-      # exclusive_caps: compatibility thing
-      # https://github.com/umlaeute/v4l2loopback
-      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-    '';
   };
 }
--- nixos/common/system/peripherals.nix
+++ nixos/common/system/peripherals.nix
@@ -1,4 +1,4 @@
-_: {
+{config, ...}: {
   # ZSA Voyager
   services.udev.extraRules = ''
     # Rules for Oryx web flashing and live training
@@ -15,4 +15,14 @@ _: {
     mountOnMedia = true;
   };
   programs.gnome-disks.enable = true;
+  # Virtual webcam
+  kernelModules = ["v4l2loopback"];
+  # TODO ?? no fucking idea what this one does, probably doesn't work without it
+  # TODO the obs guide has something slightly different?? https://nixos.wiki/wiki/OBS_Studio
+  extraModulePackages = with config.boot.kernelPackages; [v4l2loopback.out];
+  extraModprobeConfig = ''
+    # exclusive_caps: compatibility thing
+    # https://github.com/umlaeute/v4l2loopback
+    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  '';
 }

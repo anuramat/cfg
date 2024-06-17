2dddaa479b95ead548f7e3faa8d7c6009068bfc3
Arsen Nuramatov <anuramat@pm.me>
2024-06-17 02:01:06 +0200

    auto

--- nixos/common/system/boot.nix
+++ nixos/common/system/boot.nix
@@ -1,12 +1,12 @@
-{config, ...}: {
+_: {
   boot = {
-    # silent boot
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = ["quiet" "udev.log_level=3"];
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    # silent boot
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = ["quiet" "udev.log_level=3"];
   };
 }

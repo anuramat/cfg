498285790beccd98de3e9714258600c65cf786ff
Arsen Nuramatov <anuramat@pm.me>
2024-06-28 15:33:34 +0200

    auto

--- nixos/common/system/misc.nix
+++ nixos/common/system/misc.nix
@@ -28,13 +28,15 @@
   programs.seahorse.enable = true;
 
   # virtual webcam
-  kernelModules = ["v4l2loopback"];
-  # TODO ?? no fucking idea what this one does, probably doesn't work without it
-  # TODO the obs guide has something slightly different?? https://nixos.wiki/wiki/OBS_Studio
-  extraModulePackages = with config.boot.kernelPackages; [v4l2loopback.out];
-  extraModprobeConfig = ''
-    # exclusive_caps: compatibility thing
-    # https://github.com/umlaeute/v4l2loopback
-    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-  '';
+  boot = {
+    kernelModules = ["v4l2loopback"];
+    # TODO ?? no fucking idea what this one does, probably doesn't work without it
+    # TODO the obs guide has something slightly different?? https://nixos.wiki/wiki/OBS_Studio
+    extraModulePackages = with config.boot.kernelPackages; [v4l2loopback.out];
+    extraModprobeConfig = ''
+      # exclusive_caps: compatibility thing
+      # https://github.com/umlaeute/v4l2loopback
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    '';
+  };
 }

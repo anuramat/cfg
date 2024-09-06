d27d17b6156f0c4864aa55f58815058576e57311
Arsen Nuramatov <anuramat@pm.me>
2024-09-06 06:23:23 +0200

    auto

--- nixos/common/system/boilerplate.nix
+++ nixos/common/system/boilerplate.nix
@@ -5,13 +5,15 @@
     experimental-features = ["nix-command" "flakes"];
     substituters = [
       "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org"
       "https://devenv.cachix.org"
+      "https://nix-community.cachix.org"
       "https://nixpkgs-python.cachix.org"
     ];
     trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   }; # kinda imperative
   nixpkgs.config = {

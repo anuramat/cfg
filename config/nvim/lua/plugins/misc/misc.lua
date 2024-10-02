f9e4580b2753ebfd65e27af95eb619f01afb2fd7
Arsen Nuramatov <anuramat@pm.me>
2024-10-02 06:38:36 +0200

    auto

--- config/nvim/lua/plugins/misc/misc.lua
+++ config/nvim/lua/plugins/misc/misc.lua
@@ -1,18 +1,132 @@
 -- vim: fdl=1
 
 return {
-  -- sleuth - autodetects indentation settings
+  -- vim-table-mode -  tables for markdown etc
+  {
+    'dhruvasagar/vim-table-mode',
+    init = function()
+      vim.g.table_mode_disable_mappings = 1
+      vim.g.table_mode_disable_tableize_mappings = 1
+      vim.keymap.set('n', '<leader>T', '<cmd>TableModeToggle<cr>', { silent = true, desc = 'Table Mode: Toggle' })
+      vim.keymap.set(
+        'n',
+        '<leader>t',
+        '<cmd>TableModeRealign<cr>',
+        { silent = true, desc = 'Table Mode: Realign once' }
+      )
+    end,
+    ft = 'markdown',
+  },
+  -- nvim-surround - standard surround plugin
+  {
+    -- The most popular surround plugin (right after tpope/vim-surround)
+    'kylechui/nvim-surround',
+    opts = {
+      keymaps = {
+        insert = false,
+        insert_line = false,
+        -- normal = '<leader>s',
+        -- normal_cur = '<leader>ss',
+        -- normal_line = '<leader>S',
+        -- normal_cur_line = '<leader>SS',
+        -- visual = '<leader>s',
+        -- visual_line = '<leader>S',
+        delete = 'ds',
+        change = 'cs',
+        change_line = 'cS',
+      },
+    },
+    event = 'BufEnter',
+  },
+  -- mini.align - align text interactively
+  {
+    'echasnovski/mini.align',
+    -- See also:
+    -- junegunn/vim-easy-align
+    -- godlygeek/tabular
+    -- tommcdo/vim-lion
+    -- Vonr/align.nvim
+    opts = {
+      mappings = {
+        start = '<leader>a',
+        start_with_preview = '<leader>A',
+      },
+    },
+    keys = {
+      { mode = { 'x', 'n' }, '<leader>a', desc = 'Align' },
+      { mode = { 'x', 'n' }, '<leader>A', desc = 'Interactive align' },
+    },
+  },
+  -- vim-dadbod - never used, might be broken
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod' },
+      {
+        'kristijanhusak/vim-dadbod-completion',
+        ft = { 'sql', 'mysql', 'plsql' },
+        dependencies = { 'hrsh7th/nvim-cmp' },
+        init = function()
+          -- untested
+          vim.cmd(
+            [[autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })]]
+          )
+        end,
+      },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  -- vim-fetch - allows for ':e file.txt:1337'
+  {
+    lazy = false,
+    'wsdjeg/vim-fetch',
+  },
+  -- harpoon - project-local file marks
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = function()
+      local harpoon = require('harpoon')
+      local set = function(lhs, rhs, desc)
+        vim.keymap.set('n', '<leader>h' .. lhs, rhs, { silent = true, desc = 'Harpoon: ' .. desc })
+      end
+   -- stylua: ignore start
+    set('a', function() harpoon:list():add() end, 'Add')
+    set('l', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, 'List')
+    set('n', function() harpoon:list():next() end, 'Next')
+    set('p', function() harpoon:list():prev() end, 'Previous')
+      -- stylua: ignore end
+      for i = 1, 9 do
+        local si = tostring(i)
+        set(si, function()
+          harpoon:list():select(i)
+        end, 'Go to #' .. si)
+      end
+    end,
+  },
+  -- vim-sleuth - autodetects indentation settings
   {
     'tpope/vim-sleuth',
     lazy = false,
   },
-  -- colorizer - highlights eg #012345
+  -- nvim-colorizer.lua - highlights eg #012345
   {
     'NvChad/nvim-colorizer.lua',
     event = 'VeryLazy',
     opts = {},
   },
-  -- bracketed - new ]/[ targets
+  -- mini.bracketed - new ]/[ targets
   {
     'echasnovski/mini.bracketed',
     lazy = false,
@@ -26,12 +140,12 @@ return {
     build = 'sh install.sh',
     opts = {},
   },
-  -- gnu info browser
+  -- info.vim - gnu info browser
   {
     'HiPhish/info.vim',
     event = 'VeryLazy',
   },
-  -- flash
+  -- flash.nvim - jump around
   {
     'folke/flash.nvim',
     opts = {

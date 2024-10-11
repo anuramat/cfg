b8dab652645e24ac3f520bf4aa23b38ec406f971
Arsen Nuramatov <anuramat@pm.me>
2024-10-12 00:51:45 +0200

    auto

--- config/sh/fzf_previewer.sh
+++ config/sh/fzf_previewer.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2086
-# $1 is unquoted on purpose
-# TODO why...
 
-if [ -d $1 ]; then
-	# preview the directory tree
+if [ -d "$1" ]; then
+	# preview the directory tree: eza > tree > ls
 	if command -v "eza" >/dev/null 2>&1; then
-		$EZACMD --tree $1
+		$EZACMD --tree "$1"
 		exit
 	fi
 	if command -v "tree" >/dev/null 2>&1; then
-		tree -C $1
+		tree -C "$1"
 		exit
 	fi
-	ls -a $1
+	ls -a "$1"
 	exit
 
-elif [ -f $1 ]; then
-	# preview file contents
+elif [ -f "$1" ]; then
+	# preview file contents: bat > cat
 	if command -v "bat" >/dev/null 2>&1; then
-		bat -n --color=always $1
+		bat -n --color=always "$1"
 		exit
 	fi
-
-	cat $1
+	cat "$1"
 	exit
 fi

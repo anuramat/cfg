454a258ecd3003d9915bca22945137507a24543d
Arsen Nuramatov <anuramat@pm.me>
2024-11-19 14:51:16 +0100

    auto

--- config/nvim/lua/plugins/adapters/lsp.lua
+++ config/nvim/lua/plugins/adapters/lsp.lua
@@ -16,7 +16,7 @@ end
 -- <https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md>
 --- Returns configs for specific lsps
 --- @return table configs
-return function()
+local configs = function()
   return {
     nixd = {
       cmd = { 'nixd', '--inlay-hints=false' },
@@ -114,3 +114,45 @@ return function()
     },
   }
 end
+
+return {
+  'neovim/nvim-lspconfig',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = {
+    'hrsh7th/nvim-cmp',
+    'ray-x/lsp_signature.nvim',
+    'lukas-reineke/lsp-format.nvim',
+  },
+  config = function()
+    local lspconfig = require('lspconfig')
+    local configs = require('plugins.adapters.lsp.configs')
+    local on_attach = require('utils').on_attach
+    -- ~~~~~~~~~~~~~~~~ Borders styling ~~~~~~~~~~~~~~~~~ --
+    -- add border to `:LspInfo` menu
+    require('lspconfig.ui.windows').default_options.border = vim.g.border
+    -- -- add border to default hover handler (replaced by folke/noice.nvim)
+    -- vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = vim.g.border })
+    -- -- add border to default signature help (replaced with ray-x/lsp_signature.nvim)
+    -- vim.lsp.handlers['textDocument/signatureHelp'] =
+    --   vim.lsp.with(vim.lsp.handlers.signature_help, { border = vim.g.border })
+    -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ --
+    -- Register capabilities for CMP
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+    -- ~~~~~~~~~~~~~~~~ Set up servers ~~~~~~~~~~~~~~~~~ --
+    for name, cfg in pairs(configs()) do
+      cfg.capabilities = capabilities
+      if cfg.on_attach == nil then
+        cfg.on_attach = on_attach
+      end
+      lspconfig[name].setup(cfg)
+    end
+
+    require('lsp_signature').setup({
+      handler_opts = { border = vim.g.border },
+      always_trigger = true,
+      hint_enable = false, -- virtual hint enable
+      hint_prefix = '🐼 ', -- Panda for parameter (hehe)
+    })
+  end,
+}

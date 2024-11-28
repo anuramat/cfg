e9ecd5d6bb2810db70472f489132ac3531382500
Arsen Nuramatov <anuramat@pm.me>
2024-11-28 22:23:50 +0100

    auto

--- nixos/common/software/desktop.nix
+++ nixos/common/software/desktop.nix
@@ -1,62 +1,56 @@
+# vim: fdl=0 fdm=marker
 {
   pkgs,
   unstable,
   ...
 }: {
   environment.systemPackages = with pkgs; [
-    # Browsers
+    # Desktop basics {{{1
+    waybar # status bar
+    mako # notifications - smaller than fnott and dunst
+    swaylock # lockscreen
+    swayidle # lock/sleep on idle
+
+    # Browsers {{{1
     tor-browser-bundle-bin
     unstable.firefox
     unstable.google-chrome
 
-    # terminals {{{1
+    # Terminals {{{1
     cool-retro-term
     unstable.foot # cpu
     unstable.alacritty # gpu, crossplatform
     unstable.kitty
 
-    # Settings
-    networkmanagerapplet # networking
-    unstable.avizo # brightness/volume control with overlay indicator
+    # Settings {{{1
     ddcutil # configure external monitors
+    helvum # pipewire patchbay
+    networkmanagerapplet # networking
     pavucontrol # gui audio configuration
     system-config-printer # printer gui
-    helvum # pipewire patchbay
     udiskie # userspace frontend for udisk2
+    unstable.avizo # brightness/volume control with overlay indicator
+    way-displays # auto manager
+    wlay # gui display configs (can output kanshi/sway/wlr-randr files)
 
-    # Opening stuff
+    # Opening stuff {{{1
     cinnamon.nemo # wayland native
     xdragon # drag and drop from terminal
     tofi # app launcher
     xdg-utils # xdg-open etc
     desktop-file-utils # update-desktop-database etc
 
-    # Big chunks
-    waybar # status bar
-    mako # notifications - smaller than fnott and dunst
-    swaylock # lockscreen
-    swayidle # lock/sleep on idle
-
-    # Debug stuff
-    wev # wayland event viewer, useful for debugging
-
-    # Terminal helpers
+    # Terminal helpers {{{1
     libnotify # notify-send etc
     wl-clipboard # wl-copy/wl-paste: copy from stdin/paste to stdout
     playerctl # cli media player controls
 
-    # Wallpaper helpers
+    # Wallpaper helpers {{{1
     swaybg # plain
     mpvpaper # video
     glpaper # shader
 
-    # Display settings
-    kanshi # daemon, reads a plaintext config
-    wlr-randr # imperative cli display settings
-    wlay # gui display configs (can output kanshi/sway/wlr-randr files)
-    way-displays # auto manager
-
-    # Screenshots and screen capture
+    # Screenshots and screen capture {{{1
     slurp # select screen region
     grim # CLI screenshot
     shotman # screenshot, with simple preview afterwards, no markup
@@ -64,8 +58,8 @@
     wf-recorder # CLI screen capture
     kooha # screen capture with basic gui
 
-    # Misc
-    wl-clip-persist
+    # Misc {{{1
+    wl-clip-persist # otherwise clipboard contents disappear on exit
     unstable.wallust # better pywal
     spotify
     unstable.keymapp # ZSA keyboard thing
@@ -75,6 +69,7 @@
     obs-studio
     steam
     hyprpicker # simple terminal color picker
+    wev # wayland event viewer, useful for debugging
 
     # Comms {{{1
     element-desktop # matrix client
@@ -84,5 +79,7 @@
     slack
     telegram-desktop
     whatsapp-for-linux # isn't this worthless TODO
+
+    # }}}
   ];
 }

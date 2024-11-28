e030efef183952792aa7e94ee2f7ea7b71dc4c89
Arsen Nuramatov <anuramat@pm.me>
2024-11-28 22:31:30 +0100

    auto

--- nixos/common/software/desktop.nix
+++ nixos/common/software/desktop.nix
@@ -6,10 +6,10 @@
 }: {
   environment.systemPackages = with pkgs; [
     # Desktop basics {{{1
-    waybar # status bar
     mako # notifications - smaller than fnott and dunst
-    swaylock # lockscreen
     swayidle # lock/sleep on idle
+    swaylock # lockscreen
+    waybar # status bar
 
     # Browsers {{{1
     tor-browser-bundle-bin
@@ -18,58 +18,57 @@
 
     # Terminals {{{1
     cool-retro-term
-    unstable.foot # cpu
     unstable.alacritty # gpu, crossplatform
+    unstable.foot # cpu
     unstable.kitty
 
     # Settings {{{1
-    ddcutil # configure external monitors
+    ddcutil # configure external monitors (eg brightness)
     helvum # pipewire patchbay
     networkmanagerapplet # networking
     pavucontrol # gui audio configuration
     system-config-printer # printer gui
     udiskie # userspace frontend for udisk2
     unstable.avizo # brightness/volume control with overlay indicator
-    way-displays # auto manager
-    wlay # gui display configs (can output kanshi/sway/wlr-randr files)
+    way-displays # auto manager, wdisplays might be useful when they get support for ipc
 
     # Opening stuff {{{1
     cinnamon.nemo # wayland native
-    xdragon # drag and drop from terminal
+    desktop-file-utils # update-desktop-database etc
     tofi # app launcher
     xdg-utils # xdg-open etc
-    desktop-file-utils # update-desktop-database etc
+    xdragon # drag and drop from terminal
 
     # Terminal helpers {{{1
     libnotify # notify-send etc
-    wl-clipboard # wl-copy/wl-paste: copy from stdin/paste to stdout
     playerctl # cli media player controls
+    wl-clipboard # wl-copy/wl-paste: copy from stdin/paste to stdout
 
     # Wallpaper helpers {{{1
-    swaybg # plain
-    mpvpaper # video
     glpaper # shader
+    mpvpaper # video
+    swaybg # plain
 
     # Screenshots and screen capture {{{1
-    slurp # select screen region
     grim # CLI screenshot
+    kooha # screen capture with basic gui
     shotman # screenshot, with simple preview afterwards, no markup
+    slurp # select screen region
     swappy # markup wrapper for grim+slurp/etc
     wf-recorder # CLI screen capture
-    kooha # screen capture with basic gui
 
     # Misc {{{1
-    wl-clip-persist # otherwise clipboard contents disappear on exit
-    unstable.wallust # better pywal
+    hyprpicker # simple terminal color picker
+    obs-studio
     spotify
+    steam
+    unstable.cheese # webcam
     unstable.keymapp # ZSA keyboard thing
     unstable.proton-pass
     unstable.transmission_4-gtk # transmission torrent client gui
-    unstable.cheese # webcam
-    obs-studio
-    steam
-    hyprpicker # simple terminal color picker
+    unstable.wallust # better pywal
     wev # wayland event viewer, useful for debugging
+    wl-clip-persist # otherwise clipboard contents disappear on exit
 
     # Comms {{{1
     element-desktop # matrix client

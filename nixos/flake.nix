37b8a98675382395ff454fac3eb78fdd7ada9315
Arsen Nuramatov <anuramat@pm.me>
2024-11-30 19:52:04 +0100

    auto

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,7 +15,9 @@
       timezone = "Europe/Berlin";
       defaultLocale = "en_US.UTF-8";
       keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPgKxqnXU0UAshEUDLcVZW6LkfMM0JE2yuhkyjXSxUI anuramat@anuramat-t480"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPgKxqnXU0UAshEUDLcVZW6LkfMM0JE2yuhkyjXSxUI anuramat-t480"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKl0YHcx+ju+3rsPerkAXoo2zI4FXRHaxzfq8mNHCiSD anuramat-iphone16"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIORDkTNsAaKxMF/VIfoI+FXvcLARbswddfqtHNkuTsxR anuramat-ll7"
       ];
     };
     unstable = import inputs.nixpkgs-unstable {

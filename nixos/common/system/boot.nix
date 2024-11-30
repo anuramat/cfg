3367fa5f3ead4f976132ea3df40a4a77b7e74d27
Arsen Nuramatov <anuramat@pm.me>
2024-11-30 21:00:09 +0100

    auto

--- nixos/common/system/boot.nix
+++ nixos/common/system/boot.nix
@@ -5,7 +5,7 @@ _: {
         enable = true;
         configurationLimit = 10;
         memtest86.enable = true;
-        # edk2-uefi-shell.enable = true; # wait for 24.11 to release
+        edk2-uefi-shell.enable = true; # wait for 24.11 to release
         netbootxyz.enable = true;
       };
       efi.canTouchEfiVariables = true;

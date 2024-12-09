7cab3c37ed79b35439813b504c90304f7a47c8c0
Arsen Nuramatov <anuramat@pm.me>
2024-12-09 21:47:37 +0100

    auto

--- config/todo/actions/tags.py
+++ config/todo/actions/tags.py
@@ -4,28 +4,32 @@ from collections import defaultdict
 import sys
 import re
 
-symbol = sys.argv[1]
-if symbol == "+":
-    symbol = "\\" + symbol
-exp = f'{symbol}\\S+'
-pattern = re.compile(exp)
+def main():
+    symbol = sys.argv[1]
+    if symbol == "+":
+        symbol = "\\" + symbol
+    exp = f'(?<!\\S){symbol}\\S+\\s*'
+    pattern = re.compile(exp)
 
-if not symbol:
-    raise ValueError()
+    if not symbol:
+        raise ValueError()
 
-tasks_by_tags=defaultdict(list)
-for task in sys.stdin:
-    tags = pattern.findall(task)
-    task = pattern.sub("", task)
-    task = task.strip()
-    for tag in tags:
-        tasks_by_tags[tag.strip()].append(task)
-    if len(tags) == 0:
-        tasks_by_tags[f'-{symbol[-1]}'].append(task)
+    tasks_by_tags=defaultdict(list)
+    for task in sys.stdin:
+        tags = pattern.findall(task)
+        task = pattern.sub("", task)
+        task = task.strip()
+        for tag in tags:
+            tasks_by_tags[tag.strip()].append(task)
+        if len(tags) == 0:
+            tasks_by_tags[f'-{symbol[-1]}'].append(task)
 
-delim = '\0'
-res = str(len(tasks_by_tags) * 2)
-for tag, tasks in tasks_by_tags.items():
-    res += f"{delim}{tag}{delim}{'\n'.join(tasks[::-1])}"
+    delim = '\0'
+    res = str(len(tasks_by_tags) * 2)
+    for tag, tasks in tasks_by_tags.items():
+        res += f"{delim}{tag}{delim}{'\n'.join(tasks[::-1])}"
 
-print(res, end='')
+    print(res, end='')
+
+if __name__ == "__main__":
+    main()

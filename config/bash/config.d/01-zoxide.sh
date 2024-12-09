cc997a2b984dc5bacda8d1223488ce2cfeaa4e13
Arsen Nuramatov <anuramat@pm.me>
2024-12-09 05:44:59 +0100

    auto

--- config/bash/config.d/01-zoxide.sh
+++ config/bash/config.d/01-zoxide.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS
+
+export _ZO_FZF_OPTS="
 --no-sort
 --exit-0
 --select-1
+--preview='$XDG_CONFIG_HOME/bash/fzf_previewer.sh {2..}'
+$FZF_DEFAULT_OPTS
 "
 export _ZO_RESOLVE_SYMLINKS="1"
 export _ZO_ECHO=1

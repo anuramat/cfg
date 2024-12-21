a718c3667ddeb1704121241f338080a3a3f74f7e
Arsen Nuramatov <anuramat@pm.me>
2024-12-21 19:19:37 +0100

    auto

--- config/git/merge_sets.sh
+++ config/git/merge_sets.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-ours=$(sort "$1")
-before=$(sort "$2")
-theirs=$(sort "$3")
+ours=$(sort -u "$1")
+before=$(sort -u "$2")
+theirs=$(sort -u "$3")
 
-common=$(printf '%s\n'"$ours" "$theirs" | sort -u)
+common=$(printf '%s\n' "$ours" "$theirs" | sort -u)
 removed=$(printf '%s' "$before" | comm -23 - <(comm -12 <(printf '%s' "$ours") <(printf '%s' "$theirs")))
-merged=$(printf '%s' "$removed" | grep -vxF -f - <(printf '%s' "$common"))
-
-printf '%s' "$merged"
+printf '%s' "$removed" | grep -vxF -f - <(printf '%s' "$common") | sort -u > "$1"

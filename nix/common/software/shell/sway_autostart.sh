95bd8f44a44b5b7b106183c595b0ee1d9ed590da
Arsen Nuramatov <anuramat@pm.me>
2024-12-24 00:21:21 +0100

    auto: anuramat-ll7

--- nix/common/software/shell/sway_autostart.sh
+++ nix/common/software/shell/sway_autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "${WAYLAND_DISPLAY}" ] \
-	&& [ "${XDG_VTNR}" -eq 1 ] \
+	&& [ "${XDG_VTNR}" = 1 ] \
 	&& command -v sway > /dev/null 2>&1; then
 	if lspci | grep -iq nvidia; then
 		exec sway --unsupported-gpu

6b82c15a3aec52b12b79bebed239be7c07910433
Arsen Nuramatov <anuramat@pm.me>
2024-12-23 18:37:44 +0100

    auto: anuramat-ll7

--- config/bash/config.d/00-makefile.sh
+++ config/bash/config.d/00-makefile.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+
+__dot() {
+	xargs -a "$XDG_CACHE_HOME/wallust/graphviz" dot -Tpng -Gdpi=300
+}
+
 deps() {
-	make root -Bnd | make2graph | dot -Tpng -Gdpi=300 | swayimg - -s fit
+	make ${1:-root} -Bnd | make2graph | __dot | swayimg - -s fit
 }
